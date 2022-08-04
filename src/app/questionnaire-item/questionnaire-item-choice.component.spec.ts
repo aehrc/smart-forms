6b131b5925b2acab4c73ebfe40da745e89f0434d
yeexianfong <yeexianfong@gmail.com>
2022-08-04 10:48:27 +0930

    Create questionnaire item choice test

---
+++ src/app/questionnaire-item/questionnaire-item-choice.component.spec.ts
@@ -0,0 +1,43 @@
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AnswerOption } from '../services/questionnaire.model';
+
+import { QuestionnaireItemChoiceComponent } from './questionnaire-item-choice.component';
+
+describe('QuestionnaireItemChoiceComponent', () => {
+  let component: QuestionnaireItemChoiceComponent;
+  let fixture: ComponentFixture<QuestionnaireItemChoiceComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({declarations: [QuestionnaireItemChoiceComponent]});
+    fixture = TestBed.createComponent(QuestionnaireItemChoiceComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should be displayed as a radio button question', () => {
+    component.answerOption = [
+      {
+        "valueCoding": {
+          "system": "266919005", "code": "Never smoked", "display": "http://snomed.info/sct"
+        }
+      }];
+    component.itemControl = "radio-button"
+    
+    
+    fixture.detectChanges();
+
+    const radioDe: DebugElement = fixture.debugElement;
+    const radioEl: HTMLElement = radioDe.nativeElement;
+    console.log(radioEl)
+    console.log(component)
+    // const bannerDe: DebugElement = fixture.debugElement;
+    // const paragraphDe = bannerDe.query(By.css('p'));
+    expect(component).toBeDefined();
+
+  });
+});
