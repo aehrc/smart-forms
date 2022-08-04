a4dddd92fcfa4b648b3649cbae6df13a2ceb8fd0
yeexianfong <yeexianfong@gmail.com>
2022-08-04 16:16:36 +0930

    Test for radio button and dropdown questions

--- src/app/questionnaire-item/questionnaire-item-choice.component.spec.ts
+++ src/app/questionnaire-item/questionnaire-item-choice.component.spec.ts
@@ -1,5 +1,6 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { QuestionnaireItemChoiceComponent } from './questionnaire-item-choice.component';
 
@@ -17,30 +18,78 @@ describe('QuestionnaireItemChoiceComponent', () => {
     expect(component).toBeDefined();
   });
 
-  it('should be displayed as a radio button question', () => {
+  it('should be displayed as a radio button', () => {
     component.item = {
-      linkId: '1',
-      type: 'choice',
-      item: [],
-      answerOption: [
-        {
-          "valueCoding": {
-            "system": "266919005", "code": "Never smoked", "display": "http://snomed.info/sct"
-          }
-        }]
-    };
-    component.itemControl = "radio-button"
+    linkId: '661dd4c2-3092-4eb0-92d6-a8b18865c4b4',
+    text: 'Eligible for health check',
+    type: 'choice',
+    item: [],
+    answerOption: [
+      {
+        "valueCoding": {
+          "code": "Y", "display": "Yes", "system": "http://terminology.hl7.org/CodeSystem/v2-0136"
+        },
+      },
+      {
+        "valueCoding": {
+          "code": "N", "display": "No", "system": "http://terminology.hl7.org/CodeSystem/v2-0136"
+        },
+      },
+      {
+        "valueCoding": {
+          "code": "NA", "display": "NA", "system": "http://terminology.hl7.org/CodeSystem/v3-NullFlavor"
+        },
+      },
+    ]
+  };
 
 
     fixture.detectChanges();
 
     const radioDe: DebugElement = fixture.debugElement;
     const radioEl: HTMLElement = radioDe.nativeElement;
+    const inputEl = radioDe.query(By.css('input'))
+    expect(inputEl.attributes.type).toBe('radio');
     console.log(radioEl)
-    console.log(component)
-    // const bannerDe: DebugElement = fixture.debugElement;
-    // const paragraphDe = bannerDe.query(By.css('p'));
-    expect(component).toBeDefined();
+  });
 
+  it('should be displayed as a dropdown', () => {
+    component.item = {
+    linkId: '3d46c42b-a10f-4805-ad9f-ae9510bdbc4e',
+    text: 'Aboriginal and Torres Strait Islander status',
+    type: 'choice',
+    item: [],
+      answerOption: [
+        {
+          "valueCoding": {
+            "code": "1", "display": "Aboriginal but not Torres Strait Islander origin", "system": "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1"
+          }
+        }, 
+        {
+          "valueCoding": {
+            "code": "2", "display": "Torres Strait Islander but not Aboriginal origin", "system": "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1"
+          }
+        },
+        {
+          "valueCoding": {
+            "code": "3", "display": "Both Aboriginal and Torres Strait Islander origin", "system": "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1"
+          }
+        },
+        {
+          "valueCoding": {
+            "code": "4", "display": "Neither Aboriginal nor Torres Strait Islander origin", "system": "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1"
+          }
+        },
+        {
+          "valueCoding": {
+            "code": "9", "display": "Not stated/inadequately described", "system": "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1"
+          }
+        }]
+    };    
+    fixture.detectChanges();
+
+    const dropdownDe: DebugElement = fixture.debugElement;
+    const dropdownEl: HTMLElement = dropdownDe.nativeElement;
+    expect(dropdownDe.query(By.css('select')))
   });
 });
