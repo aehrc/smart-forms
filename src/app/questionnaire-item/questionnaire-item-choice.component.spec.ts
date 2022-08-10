1c9ceb0eec87ac7b7e56c0818566a641e20b880c
Sean Fong <sean.fong@csiro.au>
2022-08-10 15:27:13 +0930

    Create test case for valueString options

--- src/app/questionnaire-item/questionnaire-item-choice.component.spec.ts
+++ src/app/questionnaire-item/questionnaire-item-choice.component.spec.ts
@@ -3,17 +3,54 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 
 import { QuestionnaireItemChoiceComponent } from "./questionnaire-item-choice.component";
+import { AnswerOption } from "../services/questionnaire.model";
+
+class MockedQuestionnaireItemChoiceComponent extends QuestionnaireItemChoiceComponent {
+  item = {
+    linkId: "3d46c42b-a10f-4805-ad9f-ae9510bdbc4e",
+    text: "Aboriginal and Torres Strait Islander status",
+    type: "choice",
+    item: [],
+    answerOption: [
+      {
+        valueCoding: {
+          code: "1",
+          display: "Aboriginal but not Torres Strait Islander origin",
+          system:
+            "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
+        },
+      },
+      {
+        valueCoding: {
+          code: "2",
+          display: "Torres Strait Islander but not Aboriginal origin",
+          system:
+            "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
+        },
+      },
+      {
+        valueCoding: {
+          code: "3",
+          display: "Both Aboriginal and Torres Strait Islander origin",
+          system:
+            "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
+        },
+      },
+    ] as AnswerOption[],
+  };
+}
 
 describe("QuestionnaireItemChoiceComponent", () => {
-  let component: QuestionnaireItemChoiceComponent;
-  let fixture: ComponentFixture<QuestionnaireItemChoiceComponent>;
+  let component: MockedQuestionnaireItemChoiceComponent;
+  let fixture: ComponentFixture<MockedQuestionnaireItemChoiceComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [QuestionnaireItemChoiceComponent],
+      declarations: [MockedQuestionnaireItemChoiceComponent],
     });
-    fixture = TestBed.createComponent(QuestionnaireItemChoiceComponent);
+    fixture = TestBed.createComponent(MockedQuestionnaireItemChoiceComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it("should create", () => {
@@ -21,98 +58,48 @@ describe("QuestionnaireItemChoiceComponent", () => {
   });
 
   it("should be displayed as a radio button", () => {
-    component.item = {
-      linkId: "661dd4c2-3092-4eb0-92d6-a8b18865c4b4",
-      text: "Eligible for health check",
-      type: "choice",
-      item: [],
-      answerOption: [
-        {
-          valueCoding: {
-            code: "Y",
-            display: "Yes",
-            system: "http://terminology.hl7.org/CodeSystem/v2-0136",
-          },
-        },
-        {
-          valueCoding: {
-            code: "N",
-            display: "No",
-            system: "http://terminology.hl7.org/CodeSystem/v2-0136",
-          },
-        },
-        {
-          valueCoding: {
-            code: "NA",
-            display: "NA",
-            system: "http://terminology.hl7.org/CodeSystem/v3-NullFlavor",
-          },
-        },
-      ],
-    };
-
-    fixture.detectChanges();
-
     const radioDe: DebugElement = fixture.debugElement;
-    const radioEl: HTMLElement = radioDe.nativeElement;
     const inputEl = radioDe.query(By.css("input"));
     expect(inputEl.attributes.type).toBe("radio");
-    console.log(radioEl);
   });
 
   it("should be displayed as a dropdown", () => {
-    component.item = {
-      linkId: "3d46c42b-a10f-4805-ad9f-ae9510bdbc4e",
-      text: "Aboriginal and Torres Strait Islander status",
-      type: "choice",
-      item: [],
-      answerOption: [
-        {
-          valueCoding: {
-            code: "1",
-            display: "Aboriginal but not Torres Strait Islander origin",
-            system:
-              "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
-          },
-        },
-        {
-          valueCoding: {
-            code: "2",
-            display: "Torres Strait Islander but not Aboriginal origin",
-            system:
-              "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
-          },
-        },
-        {
-          valueCoding: {
-            code: "3",
-            display: "Both Aboriginal and Torres Strait Islander origin",
-            system:
-              "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
-          },
-        },
-        {
-          valueCoding: {
-            code: "4",
-            display: "Neither Aboriginal nor Torres Strait Islander origin",
-            system:
-              "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
-          },
-        },
-        {
-          valueCoding: {
-            code: "9",
-            display: "Not stated/inadequately described",
-            system:
-              "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
-          },
-        },
-      ],
-    };
+    component.item.answerOption.push({
+      valueCoding: {
+        code: "4",
+        display: "Neither Aboriginal nor Torres Strait Islander origin",
+        system:
+          "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
+      },
+    });
+    component.item.answerOption.push({
+      valueCoding: {
+        code: "5",
+        display: "Not stated/inadequately described",
+        system:
+          "https://healthterminologies.gov.au/fhir/CodeSystem/australian-indigenous-status-1",
+      },
+    });
+    component.answerOption = [];
+    component.onInit();
     fixture.detectChanges();
 
     const dropdownDe: DebugElement = fixture.debugElement;
-    const dropdownEl: HTMLElement = dropdownDe.nativeElement;
-    expect(dropdownDe.query(By.css("select")));
+    expect(dropdownDe.query(By.css("select"))).toBeTruthy();
+  });
+
+  it("should be displaying a dropdown value from a valueString as intended", () => {
+    component.item.answerOption[0] = {
+      valueString: "testValue",
+    };
+
+    component.answerOption = [];
+    component.onInit();
+    fixture.detectChanges();
+
+    const radioDe: DebugElement = fixture.debugElement;
+    const inputDe: DebugElement = radioDe.query(By.css("input"));
+    expect(inputDe.attributes.type).toBe("radio");
+    expect(inputDe.nativeElement.value).toBe("testValue");
   });
 });
