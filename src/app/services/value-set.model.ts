600a384084f2f595bc2b72d30c304c3720afb314
Sean Fong <sean.fong@csiro.au>
2022-08-16 10:14:55 +0930

    Add additional imports in ValueSet model

--- src/app/services/value-set.model.ts
+++ src/app/services/value-set.model.ts
@@ -1,5 +1,19 @@
 import { fhirclient } from "fhirclient/lib/types";
 
 export interface ValueSet extends fhirclient.FHIR.Resource {
+  name: string;
+  url: fhirclient.FHIR.uri;
   resourceType: "ValueSet";
+  expansion?: ValueSetExpansion;
+}
+
+export interface ValueSetExpansion extends fhirclient.FHIR.BackboneElement {
+  identifier: fhirclient.FHIR.uri;
+  contains: ValueSetCoding[];
+}
+
+export interface ValueSetCoding extends fhirclient.FHIR.BackboneElement {
+  system: fhirclient.FHIR.uri;
+  code: fhirclient.FHIR.code;
+  display: string;
 }
