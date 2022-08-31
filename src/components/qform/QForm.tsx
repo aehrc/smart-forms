8f76b6887c5aec3994a843936a551b1c95c31b99
Sean Fong <sean.fong@csiro.au>
2022-08-31 16:37:33 +0930

    Add questionnaireResponse state to QForm

--- src/components/qform/QForm.tsx
+++ src/components/qform/QForm.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
-import { Stack, Typography } from '@mui/material';
-import QItems from './QItems';
-import { QuestionnaireReader } from '../questionnaire/QuestionnaireReader';
+import { Grid, Stack, Typography } from '@mui/material';
+import { QuestionnaireService } from '../questionnaire/QuestionnaireService';
+import { QuestionnaireResponseService } from '../questionnaireResponse/QuestionnaireResponseService';
+import { Questionnaire, QuestionnaireItem } from '../questionnaire/QuestionnaireModel';
 
 function QForm() {
-  const questionnaireReader = new QuestionnaireReader();
-  questionnaireReader.printJson();
-  const questionnaireItems = questionnaireReader.questionnaire.items;
+  const qService = new QuestionnaireService();
+  const qrService = new QuestionnaireResponseService();
+
+  const [qResponse, setQResponse] = useState(qrService.questionnaireResponse);
+  const questionnaire: Questionnaire = qService.questionnaire;
 
   return (
     <div>
       <Container>
-        <Stack spacing={4} sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" sx={{ mb: 4 }}>
-            MBS715
-          </Typography>
-
-          <QItems items={questionnaireItems}></QItems>
-        </Stack>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Stack spacing={4} sx={{ my: 4 }}>
+              <Typography variant="h4" component="h1" sx={{ mb: 4 }}>
+                {questionnaire.title}
+              </Typography>
+              {questionnaire.item.map((item: QuestionnaireItem) => {
+                return <div key={item.linkId}>{/*TODO insert QGroup*/}</div>;
+              })}
+            </Stack>
+          </Grid>
+          <Grid item xs={4}>
+            {<pre>{JSON.stringify(qResponse, null, 2)}</pre>}
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
