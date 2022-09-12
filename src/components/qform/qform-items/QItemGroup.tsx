3df4abba6385c9fefa8725bec88e253a99d6427e
Sean Fong <sean.fong@csiro.au>
2022-09-12 10:40:17 +0930

    Fix group unable to be load

--- src/components/qform/qform-items/QItemGroup.tsx
+++ src/components/qform/qform-items/QItemGroup.tsx
@@ -8,6 +8,7 @@ import { QuestionnaireResponseService } from '../../questionnaireResponse/Questi
 import QItemSwitcher from './QItemSwitcher';
 import { getQrItemsIndex, mapQItemsIndex } from '../IndexFunctions';
 import QItemRepeats from '../qform-advanced-rendering/QItemRepeat';
+import { isHidden } from './QItemFunctions';
 
 interface Props
   extends PropsWithQrItemChangeHandler<QuestionnaireResponseItem>,
@@ -21,13 +22,16 @@ function QItemGroup(props: Props) {
   const qItemsIndexMap: Record<string, number> = mapQItemsIndex(qItem);
 
   const qItems = qItem.item;
-  const qrGroup = qrItem ? qrItem : QuestionnaireResponseService.createQrGroup(qItem);
+  const qrGroup =
+    qrItem && qrItem.item ? qrItem : QuestionnaireResponseService.createQrGroup(qItem);
   const qrItems = qrGroup.item;
 
   function handleQrItemChange(newQrItem: QuestionnaireResponseItem) {
     QuestionnaireResponseService.updateLinkedItem(newQrItem, qrGroup, qItemsIndexMap);
     onQrItemChange(qrGroup);
   }
+
+  if (isHidden(qItem)) return null;
 
   if (qItems && qrItems) {
     const qrItemsByIndex = getQrItemsIndex(qItems, qrItems);
