fce55052f9ed03e41cdad833e23b5a5453168d89
Sean Fong <sean.fong@csiro.au>
2022-09-14 16:57:06 +0930

    Remove rendering for non-tabbed groups if the form contains tabs

--- src/components/qform/QFormBodyTabs.tsx
+++ src/components/qform/QFormBodyTabs.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { Box, Container, Grid, Tab, Typography } from '@mui/material';
 import { QuestionnaireItem, QuestionnaireResponseItem } from 'fhir/r5';
-import { PropsWithQrItemChangeHandler, QItemType } from './FormModel';
+import { PropsWithQrItemChangeHandler } from './FormModel';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { QuestionnaireResponseService } from '../questionnaireResponse/QuestionnaireResponseService';
 import { getQrItemsIndex, mapQItemsIndex } from './IndexFunctions';
 import QItemGroup from './qform-items/QItemGroup';
-import QItemSwitcher from './qform-items/QItemSwitcher';
 import { isTab } from './TabFunctions';
 
 interface Props extends PropsWithQrItemChangeHandler<QuestionnaireResponseItem> {
@@ -32,7 +31,6 @@ function QItemBodyTabbed(props: Props) {
 
     return (
       <div>
-        {/* render tabbed groups */}
         <Container sx={{ p: 3, border: 1 }}>
           <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
             {qForm.text}
@@ -81,34 +79,6 @@ function QItemBodyTabbed(props: Props) {
               </Grid>
             </Box>
           </TabContext>
-        </Container>
-
-        {/* render untabbed groups */}
-        <Container sx={{ my: 2, p: 3, border: 1 }}>
-          {qFormItems.map((qItem: QuestionnaireItem, i) => {
-            if (!isTab(qItem)) {
-              const qrItem = qrFormItemsByIndex[i];
-              if (qItem.type === QItemType.Group) {
-                return (
-                  <QItemGroup
-                    key={qItem.linkId}
-                    qItem={qItem}
-                    qrItem={qrItem}
-                    repeats={qItem.repeats ?? false}
-                    onQrItemChange={handleQrGroupChange}></QItemGroup>
-                );
-              } else {
-                return (
-                  <QItemSwitcher
-                    key={qItem.linkId}
-                    qItem={qItem}
-                    qrItem={qrItem}
-                    repeats={qItem.repeats ?? false}
-                    onQrItemChange={handleQrGroupChange}></QItemSwitcher>
-                );
-              }
-            }
-          })}
         </Container>
       </div>
     );
