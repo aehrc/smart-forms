18d5eddd814225b9297f077f4af0cff9cd185a08
Sean Fong <sean.fong@csiro.au>
2022-09-21 23:35:16 +0930

    Reveal form when patient is obtained

--- src/components/qform/QPage.tsx
+++ src/components/qform/QPage.tsx
@@ -1,12 +1,18 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import QForm from './QForm';
 import ProgressSpinner from './ProgressSpinner';
 import { PatientContext } from '../../App';
 
 function QPage() {
+  const [loading, setLoading] = useState(true);
+
   const patient = useContext(PatientContext);
 
-  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (patient) {
+      setLoading(false);
+    }
+  }, [patient]);
 
   const renderQPage = loading ? <ProgressSpinner /> : <QForm />;
 
