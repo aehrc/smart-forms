2bed23e3492bb1abf68b185a3a5e031dc40232db
Sean Fong <sean.fong@csiro.au>
2022-09-23 16:54:48 +0930

    remove extra div from Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,37 +4,35 @@ import ResourcePicker from './ResourcePicker';
 
 function Home() {
   return (
-    <div>
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
+    <Container maxWidth="lg">
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            minHeight="85vh">
+            <Typography variant="h2" fontSize={72} fontWeight="bold" sx={{ mb: 3 }}>
+              SMART Health Checks
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Card elevation={1} sx={{ my: 15, mx: 6 }}>
             <Box
               display="flex"
               flexDirection="column"
-              alignItems="center"
               justifyContent="center"
-              minHeight="85vh">
-              <Typography variant="h2" fontSize={72} fontWeight="bold" sx={{ mb: 3 }}>
-                SMART Health Checks
-              </Typography>
+              minHeight="65vh"
+              sx={{ p: 8 }}>
+              <ResourcePicker></ResourcePicker>
             </Box>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Card elevation={1} sx={{ my: 15, mx: 6 }}>
-              <Box
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                minHeight="65vh"
-                sx={{ p: 8 }}>
-                <ResourcePicker></ResourcePicker>
-              </Box>
-            </Card>
-          </Grid>
+          </Card>
         </Grid>
-      </Container>
-    </div>
+      </Grid>
+    </Container>
   );
 }
 
