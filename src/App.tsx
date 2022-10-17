43678319e8be3e2f83d02be0d85f23ea62b09f07
Sean Fong <sean.fong@csiro.au>
2022-10-17 18:22:04 +1030

    Add App file changes - missing from last commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import './App.css';
 import { CssBaseline, useMediaQuery } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import getTheme from './theme';
-import QRenderer from './components/QRenderer/QRenderer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Launch from './components/LaunchPage/Launch';
 import QuestionnairePicker from './components/QuestionnairePicker/QuestionnairePicker';
 import { QuestionnaireProvider } from './classes/QuestionnaireProvider';
 import FhirClientProvider from './custom-contexts/FhirClientContext';
+import QAuth from './components/QRenderer/QAuth';
+import { QuestionnaireResponseProvider } from './classes/QuestionnaireResponseProvider';
 
 const questionnaireProvider = new QuestionnaireProvider();
 questionnaireProvider.readCalculatedExpressionsAndEnableWhenItems();
 questionnaireProvider.readVariables();
+
+const questionnaireResponseProvider = new QuestionnaireResponseProvider();
 
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -22,13 +25,25 @@ function App() {
         <CssBaseline />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<QRenderer questionnaireProvider={questionnaireProvider} />} />
+            <Route
+              path="/"
+              element={
+                <QAuth
+                  questionnaireProvider={questionnaireProvider}
+                  questionnaireResponseProvider={questionnaireResponseProvider}
+                />
+              }
+            />
             <Route
               path="/picker"
-              element={<QuestionnairePicker questionnaireProvider={questionnaireProvider} />}
+              element={
+                <QuestionnairePicker
+                  questionnaireProvider={questionnaireProvider}
+                  questionnaireResponseProvider={questionnaireResponseProvider}
+                />
+              }
             />
             <Route path="/launch" element={<Launch />} />
-            <Route path="*" element={<QRenderer questionnaireProvider={questionnaireProvider} />} />
           </Routes>
         </BrowserRouter>
       </FhirClientProvider>
