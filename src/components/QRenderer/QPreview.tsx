b55cec8f45234323525dfb2fd00a89623c4fe591
Sean Fong <sean.fong@csiro.au>
2022-10-19 13:20:21 +1030

    Add icons on print preview buttons

--- src/components/QRenderer/QPreview.tsx
+++ src/components/QRenderer/QPreview.tsx
@@ -5,6 +5,8 @@ import { qrToHTML } from '../../functions/PreviewFunctions';
 import { QuestionnaireProvider } from '../../classes/QuestionnaireProvider';
 import { QuestionnaireResponseProvider } from '../../classes/QuestionnaireResponseProvider';
 import { useNavigate } from 'react-router-dom';
+import EditIcon from '@mui/icons-material/Edit';
+import PrintIcon from '@mui/icons-material/Print';
 
 interface Props {
   questionnaireProvider: QuestionnaireProvider;
@@ -28,19 +30,26 @@ function QPreview(props: Props) {
 
   return (
     <Container maxWidth="lg">
-      <Box displayPrint="none">
-        <Button
-          variant="contained"
-          onClick={() => window.print()}
-          sx={{ borderRadius: 3, fontSize: 16, textTransform: 'Capitalize' }}>
-          Print
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => navigate(`/`)}
-          sx={{ borderRadius: 3, fontSize: 16, textTransform: 'Capitalize' }}>
-          Edit
-        </Button>
+      <Box displayPrint="none" display="flex" flexDirection={'row'}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Button
+            variant="contained"
+            onClick={() => window.print()}
+            sx={{ fontSize: 16, textTransform: 'Capitalize' }}>
+            Print
+            <PrintIcon sx={{ ml: 1 }} />
+          </Button>
+        </Box>
+
+        <Box>
+          <Button
+            variant="contained"
+            onClick={() => navigate(`/`)}
+            sx={{ fontSize: 16, textTransform: 'Capitalize' }}>
+            Edit
+            <EditIcon sx={{ ml: 1 }} />
+          </Button>
+        </Box>
       </Box>
       <Box sx={{ my: 3 }}>
         <div>{parsed}</div>
