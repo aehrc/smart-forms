0f3c71598f4d3ee6854fe4cabe4de3822ff02248
Sean Fong <sean.fong@csiro.au>
2022-10-20 19:21:22 +1030

    Get answerOptions from contained valueSet

--- src/custom-hooks/useValueSetOptions.ts
+++ src/custom-hooks/useValueSetOptions.ts
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Coding, QuestionnaireItem, ValueSet } from 'fhir/r5';
 import { AnswerValueSet } from '../classes/AnswerValueSet';
+import { ContainedValueSetContext } from '../components/QRenderer/QForm';
 
 function useValueSetOptions(qItem: QuestionnaireItem) {
+  const containedValueSetContext = React.useContext(ContainedValueSetContext);
   const [options, setOptions] = useState<Coding[]>([]);
 
   // get options from answerValueSet on render
@@ -17,16 +19,27 @@ function useValueSetOptions(qItem: QuestionnaireItem) {
       return;
     }
 
-    // get options from ontoserver and cache them for future use
-    AnswerValueSet.expand(valueSetUrl, (newOptions: ValueSet) => {
-      const contains = newOptions.expansion?.contains;
-      if (contains) {
-        const answerOptions = AnswerValueSet.getValueCodings(contains);
-        AnswerValueSet.cache[valueSetUrl] = answerOptions;
-        setOptions(answerOptions);
-      }
-    });
+    if (valueSetUrl.startsWith('#')) {
+      // get options from referenced valueSet
+      const reference = valueSetUrl.slice(1);
+      const valueSet = containedValueSetContext[reference];
+      setOptionsFromValueSet(valueSetUrl, valueSet);
+    } else {
+      // get options from terminology server
+      AnswerValueSet.expand(valueSetUrl, (valueSet: ValueSet) => {
+        setOptionsFromValueSet(valueSetUrl, valueSet);
+      });
+    }
   }, [qItem]);
+
+  function setOptionsFromValueSet(valueSetUrl: string, valueSet: ValueSet) {
+    const contains = valueSet.expansion?.contains;
+    if (contains) {
+      const answerOptions = AnswerValueSet.getValueCodings(contains);
+      AnswerValueSet.cache[valueSetUrl] = answerOptions;
+      setOptions(answerOptions);
+    }
+  }
 
   return [options];
 }
