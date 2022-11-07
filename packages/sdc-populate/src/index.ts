f82e92679b79e29fc055fe30bb44fb888ed0718b
Sean Fong <sean.fong@csiro.au>
2022-11-07 12:08:53 +1030

    Set up sdc-populate package in main renderer app

---
+++ packages/sdc-populate/src/index.ts
@@ -0,0 +1,7 @@
+import questionnaire from "./resources/715.R4.json";
+import patient from "../src/resources/patient.json";
+import batchResponse from "../src/resources/batchresponse.json";
+
+export function populate() {
+  console.log("testttt");
+}
