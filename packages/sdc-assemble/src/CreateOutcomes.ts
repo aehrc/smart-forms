4bdc122d0178c0927ac3e05e6430a079deebb07c
Sean Fong <sean.fong@csiro.au>
2022-12-19 00:28:13 +1030

    Remove specific createOperationOutcome functions

--- packages/sdc-assemble/src/CreateOutcomes.ts
+++ packages/sdc-assemble/src/CreateOutcomes.ts
@@ -1,42 +1,6 @@
 import type { OperationOutcome } from 'fhir/r5';
 
 /**
- * Return an OperationOutcome for invalid master questionnaire items
- *
- * @author Sean Fong
- */
-export function createInvalidMasterQuestionnaireOutcome(): OperationOutcome {
-  return {
-    resourceType: 'OperationOutcome',
-    issue: [
-      {
-        severity: 'error',
-        code: 'invalid',
-        details: { text: 'Master questionnaire does not have a valid item.' }
-      }
-    ]
-  };
-}
-
-/**
- * Return an OperationOutcome for invalid subquestionnaires
- *
- * @author Sean Fong
- */
-export function createInvalidSubquestionnairesOutcome(): OperationOutcome {
-  return {
-    resourceType: 'OperationOutcome',
-    issue: [
-      {
-        severity: 'error',
-        code: 'invalid',
-        details: { text: 'Master questionnaire contains an invalid subquestionnaire.' }
-      }
-    ]
-  };
-}
-
-/**
  * Return an OperationOutcome with a supplied errorMessage
  *
  * @author Sean Fong
