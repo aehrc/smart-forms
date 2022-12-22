a3a95b979ccc042a0305db5a31ceaa42e11cbb1f
Sean Fong <sean.fong@csiro.au>
2022-12-22 12:18:54 +1030

    Escape dollar sign from /fhir/$assemble get url

--- services/assemble-express/src/index.ts
+++ services/assemble-express/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+
 const app = express();
 const port = 3002;
 
-app.get('/fhir/$assemble', (_, res) => {
+app.get('/fhir/\\$assemble', (_, res) => {
   res.send('Hello world from assemble-express!');
 });
 
