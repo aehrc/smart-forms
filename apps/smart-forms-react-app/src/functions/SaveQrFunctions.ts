6d0e4ae4212ebc876444102bdc8fea411c5745a6
Sean Fong <sean.fong@csiro.au>
2023-01-04 10:23:26 +1030

    Remove answers from questionnaireResponse items which question is hidden - either by enableWhen or hidden attribute

--- apps/smart-forms-react-app/src/functions/SaveQrFunctions.ts
+++ apps/smart-forms-react-app/src/functions/SaveQrFunctions.ts
@@ -1,8 +1,17 @@
-import { Patient, Practitioner, Questionnaire, QuestionnaireResponse } from 'fhir/r5';
+import {
+  Patient,
+  Practitioner,
+  Questionnaire,
+  QuestionnaireItem,
+  QuestionnaireResponse,
+  QuestionnaireResponseItem,
+  QuestionnaireResponseItemAnswer
+} from 'fhir/r5';
 import Client from 'fhirclient/lib/Client';
 import { constructName } from './LaunchContextFunctions';
 import dayjs from 'dayjs';
 import { qrToHTML } from './PreviewFunctions';
+import { hideQItem } from './QItemFunctions';
 
 /**
  * Sends a request to client CMS to write back a completed questionnaireResponse
@@ -23,7 +32,7 @@ export async function saveQuestionnaireResponse(
 
   let requestUrl = 'QuestionnaireResponse';
   let method = 'POST';
-  let questionnaireResponseBody = { ...questionnaireResponse };
+  let questionnaireResponseBody: QuestionnaireResponse = { ...questionnaireResponse };
 
   if (questionnaireResponse.id) {
     requestUrl += '/' + questionnaireResponse.id;
@@ -49,6 +58,8 @@ export async function saveQuestionnaireResponse(
     };
   }
 
+  questionnaireResponseBody = removeHiddenAnswers(questionnaire, questionnaireResponseBody);
+
   return client.request({
     url: requestUrl,
     method: method,
@@ -57,24 +68,78 @@ export async function saveQuestionnaireResponse(
   });
 }
 
-/**
- * Sends a request to client CMS to write back a completed questionnaireResponse
- *
- * @author Sean Fong
- */
-export async function updateExistingQuestionnaireResponse(
-  client: Client,
+export function removeHiddenAnswers(
+  questionnaire: Questionnaire,
   questionnaireResponse: QuestionnaireResponse
-) {
-  const headers = {
-    'Cache-Control': 'no-cache',
-    'Content-Type': 'application/json+fhir; charset=UTF-8'
-  };
+): QuestionnaireResponse {
+  const questionnaireItem = questionnaire.item;
+  const questionnaireResponseItem = questionnaireResponse.item;
+  if (
+    !questionnaireItem ||
+    questionnaireItem.length === 0 ||
+    !questionnaireResponseItem ||
+    questionnaireResponseItem.length === 0
+  ) {
+    return questionnaireResponse;
+  }
 
-  return client.request({
-    url: 'QuestionnaireResponse',
-    method: 'POST',
-    body: JSON.stringify(questionnaireResponse),
-    headers: headers
+  questionnaireResponseItem.forEach((qrItem, i) => {
+    const qItem = questionnaireItem[i];
+    const newQrForm = readQuestionnaireResponseItem(qItem, qrItem);
+    if (newQrForm && questionnaireResponse.item) {
+      questionnaireResponse.item[i] = { ...newQrForm };
+    }
   });
+
+  return questionnaireResponse;
+}
+
+function readQuestionnaireResponseItem(
+  qItem: QuestionnaireItem,
+  qrItem: QuestionnaireResponseItem
+): QuestionnaireResponseItem | null {
+  const qItems = qItem.item;
+  if (qItems && qItems.length > 0) {
+    if (hideQItem(qItem)) return null;
+
+    const qrItems = qrItem.item;
+    const qrAnswerItems = qrItem.answer;
+    if (qrItems && qrItems.length > 0) {
+      const newQrItems: QuestionnaireResponseItem[] = [];
+      for (let i = 0, j = 0; i < qItems.length; i++) {
+        if (qrItems[j] && qItems[i].linkId === qrItems[j].linkId) {
+          const newQrItem = readQuestionnaireResponseItem(qItems[i], qrItems[j]);
+          if (newQrItem) {
+            newQrItems.push(newQrItem);
+          }
+          j++;
+        }
+      }
+      return { ...qrItem, item: newQrItems };
+    } else if (qrAnswerItems && qrAnswerItems.length > 0 && qrAnswerItems[0].item) {
+      const newQrAnswers: QuestionnaireResponseItemAnswer[] = [];
+      for (let a = 0; a < qrAnswerItems.length; a++) {
+        const repeatAnswer = qrAnswerItems[a];
+        const newRepeatAnswerItems: QuestionnaireResponseItem[] = [];
+
+        if (repeatAnswer && repeatAnswer.item && repeatAnswer.item.length > 0) {
+          for (let i = 0, j = 0; i < qItems.length; i++) {
+            if (repeatAnswer.item[j] && qItems[i].linkId === repeatAnswer.item[j].linkId) {
+              const newQrItem = readQuestionnaireResponseItem(qItems[i], repeatAnswer.item[j]);
+              if (newQrItem) {
+                newRepeatAnswerItems.push(newQrItem);
+              }
+              j++;
+            }
+          }
+        }
+
+        newQrAnswers.push({ ...repeatAnswer, item: newRepeatAnswerItems });
+      }
+      return { ...qrItem, answer: newQrAnswers };
+    }
+    return qrItem;
+  }
+
+  return hideQItem(qItem) ? null : { ...qrItem };
 }
