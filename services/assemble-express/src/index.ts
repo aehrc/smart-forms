7df97e999c31479b2dc16aa6b8e75e15ff7b4c49
Sean Fong <sean.fong@csiro.au>
2023-01-16 15:44:07 +1030

    Integrate assemble function to process $assemble requests

--- services/assemble-express/src/index.ts
+++ services/assemble-express/src/index.ts
@@ -1,14 +1,34 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import assemble, { isAssembleInputParameters } from 'sdc-assemble';
+import { createOperationOutcome } from 'sdc-assemble/lib/CreateOutcomes';
 
 const app = express();
 const port = 3002;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+const jsonParser = bodyParser.json();
 
 app.get('/healthcheck', (_, res) => {
   res.send('This service is healthy!');
 });
 
-app.get('/fhir/\\$assemble', (_, res) => {
-  res.send('Hello world from assemble-express!');
+app.post('/fhir/\\$assemble', jsonParser, (req, res) => {
+  const parameters = req.body;
+  if (isAssembleInputParameters(req.body)) {
+    assemble(parameters)
+      .then((outputParams) => {
+        res.json(outputParams.parameter[0].resource);
+      })
+      .catch((err) => res.status(400).json(err));
+  } else {
+    const operationOutcome = createOperationOutcome(
+      'Parameters provided is invalid against the $assemble specification.'
+    );
+    res.status(400).json(operationOutcome);
+  }
 });
 
 app.listen(port, () => {
