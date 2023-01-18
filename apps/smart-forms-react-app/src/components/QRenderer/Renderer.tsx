fb8cb1e5d94f1215ee30474c4cb57c5ac0a9d357
Sean Fong <sean.fong@csiro.au>
2023-01-18 11:11:14 +1030

    Tweak checks for executing the populateQuestionnaire function in the renderer

--- apps/smart-forms-react-app/src/components/QRenderer/Renderer.tsx
+++ apps/smart-forms-react-app/src/components/QRenderer/Renderer.tsx
@@ -38,8 +38,11 @@ function Renderer() {
     const qrFormItem = questionnaireResponseProvider.questionnaireResponse.item?.[0].item;
 
     // if questionnaire has a contained attribute OR questionnaireResponse does not have a form item
-    if (questionnaire.contained && (!qrFormItem || qrFormItem.length === 0)) {
-      // obtain questionnaireResponse for prepopulation
+    if (
+      (questionnaire.contained || questionnaire.extension) &&
+      (!qrFormItem || qrFormItem.length === 0)
+    ) {
+      // obtain questionnaireResponse for pre-population
       populateQuestionnaire(
         client,
         questionnaire,
