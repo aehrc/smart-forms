a7b3d697a6fa03da9b474f7abd10043cd57144ba
Sean Fong <sean.fong@csiro.au>
2023-01-18 14:42:29 +1030

    Move placement of variables out from test function into describe function in ReadInitialExpressions.test.ts

--- packages/sdc-populate/src/ReadInitialExpressions.test.ts
+++ packages/sdc-populate/src/ReadInitialExpressions.test.ts
@@ -2,11 +2,11 @@ import { readInitialExpressions } from './ReadInitialExpressions';
 import type { Questionnaire } from 'fhir/r5';
 import Q715XFhirQuery from './resources/715-v.json';
 
-describe('initial expressions', () => {
-  test('getting an initialExpression with a linkId key should return an object containing its expression', () => {
-    const questionnaire = Q715XFhirQuery as Questionnaire;
-    const initialExpressions = readInitialExpressions(questionnaire);
+describe('read initial expressions', () => {
+  const questionnaire = Q715XFhirQuery as Questionnaire;
+  const initialExpressions = readInitialExpressions(questionnaire);
 
+  test('getting an initial expression with a linkId key should return an object containing its expression', () => {
     const prePopQuery0 = {
       expression:
         "%PrePopQuery0.entry.resource.select(relationship.coding.display + ' - ' + condition.code.coding.display).join(' ')"
