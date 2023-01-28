1b2e4e4941ba6b6d25c1e06804b23249f15c299c
Sean Fong <sean.fong@csiro.au>
2023-01-28 21:42:20 +1030

    Add styling to side bar icon buttons

--- apps/smart-forms-react-app/src/components/SideBar/SideBarBottom.styles.tsx
+++ apps/smart-forms-react-app/src/components/SideBar/SideBarBottom.styles.tsx
@@ -1,4 +1,4 @@
-import { Box, styled } from '@mui/material';
+import { Box, IconButton, styled } from '@mui/material';
 
 export const OrganisationLogoBox = styled(Box)(() => ({
   display: 'flex',
@@ -10,4 +10,16 @@ export const SideBarExpandButtonBox = styled(Box)(() => ({
   display: 'flex',
   justifyContent: 'end',
   padding: 3
+}));
+
+export const SideBarIconButton = styled(IconButton)(({ theme }) => ({
+  '&:hover, &.Mui-focusVisible': {
+    backgroundColor: theme.palette.accent2.light,
+    '&.MuiButtonBase-root': {
+      color: theme.palette.secondary.dark
+    }
+  },
+  '&.MuiButtonBase-root': {
+    color: '#444746'
+  }
 }));
--- apps/smart-forms-react-app/src/components/SideBar/SideBarBottom.tsx
+++ apps/smart-forms-react-app/src/components/SideBar/SideBarBottom.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import CSIROLogo from '../../data/images/CSIRO_Logo.png';
-import { Box, Grid, IconButton } from '@mui/material';
-import { OrganisationLogoBox, SideBarExpandButtonBox } from './SideBarBottom.styles';
+import logo from '../../data/images/CSIRO_Logo.png';
+import { Box, Grid } from '@mui/material';
+import {
+  OrganisationLogoBox,
+  SideBarExpandButtonBox,
+  SideBarIconButton
+} from './SideBarBottom.styles';
 import { KeyboardDoubleArrowLeft } from '@mui/icons-material';
+import { SideBarContext } from '../../custom-contexts/SideBarContext';
 
 function SideBarBottom() {
+  const sideBar = React.useContext(SideBarContext);
+
   return (
     <Grid container alignItems="center">
       <Grid item xs={4}></Grid>
@@ -16,16 +23,18 @@ function SideBarBottom() {
               maxHeight: { xs: 40 },
               maxWidth: { xs: 40 }
             }}
-            src={CSIROLogo}
+            src={logo}
           />
         </OrganisationLogoBox>
       </Grid>
 
       <Grid item xs={4}>
         <SideBarExpandButtonBox>
-          <IconButton size="small" onClick={() => console.log()}>
+          <SideBarIconButton
+            size="small"
+            onClick={() => sideBar.setIsExpanded(!sideBar.isExpanded)}>
             <KeyboardDoubleArrowLeft fontSize="small" />
-          </IconButton>
+          </SideBarIconButton>
         </SideBarExpandButtonBox>
       </Grid>
     </Grid>
