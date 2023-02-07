a6080e58f59a268041268abd5f5ffac43f01c42d
Sean Fong <sean.fong@csiro.au>
2023-02-07 14:34:01 +1030

    Add test for launch with authorisation and with a questionnaireUrl parameter

--- apps/smart-forms-react-app/cypress/e2e/launch.cy.ts
+++ apps/smart-forms-react-app/cypress/e2e/launch.cy.ts
@@ -1,9 +1,8 @@
-/// <reference types="cypress" />
-/// <reference path="../global.d.ts"/>
+import Q715 from '../../src/data/resources/715.R4.json';
 
-describe('launching app', () => {
-  context('launching without authorisation', () => {
-    it('redirects from launch page to renderer', () => {
+describe('launch app', () => {
+  context('launch without authorisation', () => {
+    it('from launch page, redirect to picker', () => {
       cy.visit('http://localhost:3000/launch');
       cy.intercept('/').as('homepage');
       cy.wait('@homepage').its('response.statusCode').should('eq', 200);
@@ -12,12 +11,14 @@ describe('launching app', () => {
     });
   });
 
-  context('launching with authorisation', () => {
+  context('launch with authorisation without questionnaire url parameter', () => {
     const patientId = 'd64b37f5-d3b5-4c25-abe8-23ebe8f5a04e';
 
-    it.only('redirects from launch page to renderer', () => {
-      cy.intercept('https://launch.smarthealthit.org/v/r4/auth/token').as('authorising');
-      cy.intercept('http://localhost:3000/').as('authPage');
+    it('from launch page, fetch patient and redirect to picker', () => {
+      cy.intercept({
+        method: 'POST',
+        url: 'https://launch.smarthealthit.org/v/r4/auth/token'
+      }).as('authorising');
       cy.intercept(
         'https://launch.smarthealthit.org/v/r4/fhir/Questionnaire?_count=10&_sort=-&'
       ).as('fetchQuestionnaire');
@@ -32,7 +33,6 @@ describe('launching app', () => {
       cy.wait('@authorising');
       cy.getByData('progress-spinner').find('.MuiTypography-root').contains('Fetching patient');
 
-      // data-test="picker-card-heading-responses"
       cy.wait('@fetchQuestionnaire').its('response.statusCode').should('eq', 200);
       cy.wait('@fetchQuestionnaireResponse').its('response.statusCode').should('eq', 200);
 
@@ -41,6 +41,61 @@ describe('launching app', () => {
       cy.getByData('picker-alert-refine').find('.MuiAlert-message').contains('Refine your search');
 
       cy.location('pathname').should('eq', '/');
+    });
+  });
+
+  context('launch with authorisation with a questionnaire url parameter', () => {
+    const questionnaireUrl = 'http://www.health.gov.au/assessments/mbs/715';
+    const questionnaireId = 'mbs715';
+    const questionnaire = Q715;
+    const smartHealthItLink =
+      'http://localhost:3000/launch?questionnaireUrl=http%3A%2F%2Fwww.health.gov.au%2Fassessments%2Fmbs%2F715&iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImQ2NGIzN2Y1LWQzYjUtNGMyNS1hYmU4LTIzZWJlOGY1YTA0ZSIsImU0NDNhYzU4LThlY2UtNDM4NS04ZDU1LTc3NWMxYjhmM2EzNyIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0';
+
+    it('from launch page, fetch patient and redirect to renderer while populating form', () => {
+      cy.intercept({
+        method: 'POST',
+        url: 'https://launch.smarthealthit.org/v/r4/auth/token'
+      }).as('authorising');
+      cy.intercept({
+        method: 'POST',
+        url: 'https://launch.smarthealthit.org/v/r4/fhir/'
+      }).as('populating');
+      cy.intercept('http://localhost:3000/').as('populatedForm');
+
+      cy.request({
+        method: 'GET',
+        url: `https://launch.smarthealthit.org/v/r4/fhir/Questionnaire?url=${questionnaireUrl}`
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('total');
+
+        // PUT questionnaire if it doesn't already exist
+        if (response.body.total === 0) {
+          console.log(response.body.total);
+          cy.request({
+            method: 'PUT',
+            url: `https://launch.smarthealthit.org/v/r4/fhir/Questionnaire/${questionnaireId}`,
+            body: questionnaire
+          }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('resourceType', 'Questionnaire');
+            expect(response.body).to.have.property('id', questionnaireId);
+          });
+        }
+      });
+
+      cy.visit(smartHealthItLink);
+
+      cy.wait('@authorising');
+      cy.getByData('progress-spinner').find('.MuiTypography-root').contains('Fetching patient');
+
+      cy.wait('@populating');
+      cy.getByData('progress-spinner')
+        .find('.MuiTypography-root')
+        .contains('Populating questionnaire form');
+
+      cy.location('pathname').should('eq', '/');
+      cy.getByData('renderer-heading').should('be.visible');
     });
   });
 });
