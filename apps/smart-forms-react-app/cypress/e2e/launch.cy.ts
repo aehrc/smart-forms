c6b7c9816f3754f17bd33b30dc708283886a2e37
Sean Fong <sean.fong@csiro.au>
2023-02-08 10:09:50 +1030

    Clean up Launch cypress tests

--- apps/smart-forms-react-app/cypress/e2e/launch.cy.ts
+++ apps/smart-forms-react-app/cypress/e2e/launch.cy.ts
@@ -1,45 +1,44 @@
 import Q715 from '../../src/data/resources/715.R4.json';
 
 describe('launch app', () => {
-  context('launch without authorisation', () => {
-    it('from launch page, redirect to picker', () => {
-      cy.visit('http://localhost:3000/launch');
-      cy.intercept('/').as('homepage');
-      cy.wait('@homepage').its('response.statusCode').should('eq', 200);
+  const clientUrl = 'https://launch.smarthealthit.org/v/r4/fhir';
 
+  context('launch without authorisation', () => {
+    const launchPage = 'http://localhost:3000/launch';
+
+    it('from launch page, redirect to picker', () => {
+      cy.intercept('/').as('homepage');
+
+      cy.visit(launchPage);
+      cy.wait('@homepage').its('response.statusCode').should('eq', 200);
       cy.location('pathname').should('eq', '/');
     });
   });
 
   context('launch with authorisation without questionnaire url parameter', () => {
     const patientId = 'd64b37f5-d3b5-4c25-abe8-23ebe8f5a04e';
+    const launchUrl =
+      'http://localhost:3000/launch?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImQ2NGIzN2Y1LWQzYjUtNGMyNS1hYmU4LTIzZWJlOGY1YTA0ZSIsImU0NDNhYzU4LThlY2UtNDM4NS04ZDU1LTc3NWMxYjhmM2EzNyIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0';
 
     it('from launch page, fetch patient and redirect to picker', () => {
       cy.intercept({
         method: 'POST',
         url: 'https://launch.smarthealthit.org/v/r4/auth/token'
       }).as('authorising');
-      cy.intercept(
-        'https://launch.smarthealthit.org/v/r4/fhir/Questionnaire?_count=10&_sort=-&'
-      ).as('fetchQuestionnaire');
-      cy.intercept(
-        `https://launch.smarthealthit.org/v/r4/fhir/QuestionnaireResponse?patient=${patientId}&`
-      ).as('fetchQuestionnaireResponse');
-
-      cy.visit(
-        'http://localhost:3000/launch?iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImQ2NGIzN2Y1LWQzYjUtNGMyNS1hYmU4LTIzZWJlOGY1YTA0ZSIsImU0NDNhYzU4LThlY2UtNDM4NS04ZDU1LTc3NWMxYjhmM2EzNyIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0'
+      cy.intercept(`${clientUrl}/Questionnaire?_count=10&_sort=-&`).as('fetchQuestionnaire');
+      cy.intercept(`${clientUrl}/QuestionnaireResponse?patient=${patientId}&`).as(
+        'fetchQuestionnaireResponse'
       );
 
+      cy.visit(launchUrl);
       cy.wait('@authorising');
       cy.getByData('progress-spinner').find('.MuiTypography-root').contains('Fetching patient');
 
       cy.wait('@fetchQuestionnaire').its('response.statusCode').should('eq', 200);
       cy.wait('@fetchQuestionnaireResponse').its('response.statusCode').should('eq', 200);
-
       cy.getByData('picker-card-heading-questionnaires').contains('Questionnaires');
       cy.getByData('picker-card-heading-responses').contains('Responses');
       cy.getByData('picker-alert-refine').find('.MuiAlert-message').contains('Refine your search');
-
       cy.location('pathname').should('eq', '/');
     });
   });
@@ -48,7 +47,7 @@ describe('launch app', () => {
     const questionnaireUrl = 'http://www.health.gov.au/assessments/mbs/715';
     const questionnaireId = 'mbs715';
     const questionnaire = Q715;
-    const smartHealthItLink =
+    const launchUrl =
       'http://localhost:3000/launch?questionnaireUrl=http%3A%2F%2Fwww.health.gov.au%2Fassessments%2Fmbs%2F715&iss=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&launch=WzAsImQ2NGIzN2Y1LWQzYjUtNGMyNS1hYmU4LTIzZWJlOGY1YTA0ZSIsImU0NDNhYzU4LThlY2UtNDM4NS04ZDU1LTc3NWMxYjhmM2EzNyIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMV0';
 
     it('from launch page, fetch patient and redirect to renderer while populating form', () => {
@@ -58,13 +57,12 @@ describe('launch app', () => {
       }).as('authorising');
       cy.intercept({
         method: 'POST',
-        url: 'https://launch.smarthealthit.org/v/r4/fhir/'
+        url: clientUrl
       }).as('populating');
-      cy.intercept('http://localhost:3000/').as('populatedForm');
 
       cy.request({
         method: 'GET',
-        url: `https://launch.smarthealthit.org/v/r4/fhir/Questionnaire?url=${questionnaireUrl}`
+        url: `${clientUrl}/Questionnaire?url=${questionnaireUrl}`
       }).then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).to.have.property('total');
@@ -74,7 +72,7 @@ describe('launch app', () => {
           console.log(response.body.total);
           cy.request({
             method: 'PUT',
-            url: `https://launch.smarthealthit.org/v/r4/fhir/Questionnaire/${questionnaireId}`,
+            url: `${clientUrl}/Questionnaire/${questionnaireId}`,
             body: questionnaire
           }).then((response) => {
             expect(response.status).to.eq(200);
@@ -84,8 +82,7 @@ describe('launch app', () => {
         }
       });
 
-      cy.visit(smartHealthItLink);
-
+      cy.visit(launchUrl);
       cy.wait('@authorising');
       cy.getByData('progress-spinner').find('.MuiTypography-root').contains('Fetching patient');
 
@@ -93,7 +90,6 @@ describe('launch app', () => {
       cy.getByData('progress-spinner')
         .find('.MuiTypography-root')
         .contains('Populating questionnaire form');
-
       cy.location('pathname').should('eq', '/');
       cy.getByData('renderer-heading').should('be.visible');
     });
