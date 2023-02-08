d4168f309e0f18dbb4c98a29552a25eee2bb205e
Sean Fong <sean.fong@csiro.au>
2023-02-08 21:40:43 +1030

    Add testing for cvd risk calculation

--- apps/smart-forms-react-app/cypress/e2e/calculations.cy.ts
+++ apps/smart-forms-react-app/cypress/e2e/calculations.cy.ts
@@ -1,13 +1,8 @@
 describe('calculations via variables', () => {
-  const clientUrl = 'https://launch.smarthealthit.org/v/r4/fhir';
   const launchPage = 'http://localhost:3000/launch';
 
   beforeEach(() => {
     cy.visit(launchPage);
-    cy.getByData('picker-questionnaire-list').find('.MuiButtonBase-root').eq(0).click();
-    cy.getByData('button-create-response').click();
-
-    cy.getByData('renderer-heading').should('be.visible');
   });
 
   it('enter weight and height inputs, then verify calculated BMI', () => {
@@ -15,15 +10,19 @@ describe('calculations via variables', () => {
     const heightInCm = 180;
     const bmi = weightInKg / Math.pow(heightInCm / 100, 2);
 
+    cy.getByData('picker-questionnaire-list').find('.MuiButtonBase-root').eq(0).click();
+    cy.getByData('button-create-response').click();
+    cy.getByData('renderer-heading').should('be.visible');
+
     cy.getByData('renderer-tab-list')
       .find('.MuiButtonBase-root')
       .eq(18)
       .contains('Examination')
       .click();
 
-    cy.getByData('q-item-decimal-field').eq(0).find('input').type(heightInCm.toString());
-    cy.getByData('q-item-decimal-field').eq(1).find('input').type(weightInKg.toString());
-    cy.getByData('q-item-decimal-field')
+    cy.getByData('q-item-decimal-box').eq(0).find('input').type(heightInCm.toString());
+    cy.getByData('q-item-decimal-box').eq(1).find('input').type(weightInKg.toString());
+    cy.getByData('q-item-decimal-box')
       .eq(2)
       .find('input')
       .should('be.disabled')
@@ -31,19 +30,55 @@ describe('calculations via variables', () => {
   });
 
   it('enter inputs for cardiovascular risk calculation, then verify calculated score', () => {
+    const age = 60;
     const systolicBP = 120;
     const totCholesterol = 6;
     const hdlCholesterol = 4;
 
+    // Use assembled 715 questionnaire
+    cy.getByData('picker-questionnaire-list').find('.MuiButtonBase-root').eq(4).click();
+    cy.getByData('button-create-response').click();
+    cy.getByData('renderer-heading').should('be.visible');
+
     cy.getByData('renderer-tab-list')
       .find('.MuiButtonBase-root')
-      .eq(19)
+      .eq(2)
+      .contains('Patient Details')
+      .click();
+
+    cy.getByData('q-item-integer-box').eq(0).find('input').type(age.toString());
+
+    cy.getByData('renderer-tab-list')
+      .find('.MuiButtonBase-root')
+      .eq(25)
       .contains('Absolute cardiovascular risk calculation')
       .click();
 
-    cy.getByData('q-item-decimal-field').eq(0).find('input').type(systolicBP.toString());
-    cy.getByData('q-item-decimal-field').eq(1).find('input').type(totCholesterol.toString());
-    cy.getByData('q-item-decimal-field').eq(2).find('input').type(hdlCholesterol.toString());
+    cy.getByData('q-item-decimal-box').eq(0).find('input').type(systolicBP.toString());
+    cy.getByData('q-item-decimal-box').eq(1).find('input').type(totCholesterol.toString());
+    cy.getByData('q-item-decimal-box').eq(2).find('input').type(hdlCholesterol.toString());
+    cy.getByData('q-item-integer-box').eq(0).find('input').should('have.value', '2');
+
+    cy.getByData('q-item-boolean-box').eq(0).find('input').check();
+    cy.getByData('q-item-integer-box').eq(0).find('input').should('have.value', '4');
+
+    cy.getByData('q-item-boolean-box').eq(1).find('input').check();
+    cy.getByData('q-item-integer-box').eq(0).find('input').should('have.value', '7');
+
+    cy.getByData('renderer-tab-list')
+      .find('.MuiButtonBase-root')
+      .eq(17)
+      .contains('Substance use, including tobacco')
+      .click();
+
+    cy.getByData('q-item-choice-radio-answer-option-box').eq(0).find('input').eq(1).check();
+
+    cy.getByData('renderer-tab-list')
+      .find('.MuiButtonBase-root')
+      .eq(25)
+      .contains('Absolute cardiovascular risk calculation')
+      .click();
+    cy.getByData('q-item-integer-box').eq(0).find('input').should('have.value', '13');
   });
 });
 
