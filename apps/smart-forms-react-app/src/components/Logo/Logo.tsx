bfccc8c86e9a7ac763621ab8dd55a872c6e973b0
Sean Fong <sean.fong@csiro.au>
2023-02-23 11:45:50 +1030

    Modify logo

--- apps/smart-forms-react-app/src/components/Logo/Logo.tsx
+++ apps/smart-forms-react-app/src/components/Logo/Logo.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react';
-import { Box, SxProps, Theme, Typography } from '@mui/material';
+import { Box, SxProps, Theme, Typography, useTheme } from '@mui/material';
 
 interface Props {
   sx?: SxProps<Theme>;
@@ -7,6 +7,8 @@ interface Props {
 
 const Logo = forwardRef((props: Props, ref) => {
   const { sx } = props;
+
+  const theme = useTheme();
 
   const logo = (
     <Box
@@ -20,7 +22,7 @@ const Logo = forwardRef((props: Props, ref) => {
       }}>
       <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24">
         <path
-          fill="#0092E4"
+          fill={`${theme.palette.primary.main}`}
           d="M18,18a1,1,0,0,0-1,1,1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V5A1,1,0,0,1,6,4h5V7a3,3,0,0,0,3,3h3v1a1,1,0,0,0,2,0V9s0,0,0-.06a1.31,1.31,0,0,0-.06-.27.32.32,0,0,0,0-.09,1.07,1.07,0,0,0-.19-.28h0l-6-6h0a1.07,1.07,0,0,0-.28-.19l-.1,0A1.14,1.14,0,0,0,12.07,2H6A3,3,0,0,0,3,5V19a3,3,0,0,0,3,3H16a3,3,0,0,0,3-3A1,1,0,0,0,18,18ZM13,5.41,15.59,8H14a1,1,0,0,1-1-1ZM20,14H17.5a1,1,0,0,0-.71.29l-1.24,1.25-2.8-3.2a1,1,0,0,0-1.46-.05L9.59,14H8a1,1,0,0,0,0,2h2a1,1,0,0,0,.71-.29L12,14.46l2.8,3.2a1,1,0,0,0,.72.34h0a1,1,0,0,0,.71-.29L17.91,16H20a1,1,0,0,0,0-2Z"
         />
       </svg>
@@ -28,14 +30,12 @@ const Logo = forwardRef((props: Props, ref) => {
   );
 
   return (
-    <>
-      <Box display="flex" alignItems="center">
-        {logo}
-        <Typography variant="h6" sx={{ ml: 1.5 }}>
-          SMART Forms
-        </Typography>
-      </Box>
-    </>
+    <Box display="flex" alignItems="center">
+      {logo}
+      <Typography variant="h6" sx={{ ml: 1 }}>
+        SMART Forms
+      </Typography>
+    </Box>
   );
 });
 
