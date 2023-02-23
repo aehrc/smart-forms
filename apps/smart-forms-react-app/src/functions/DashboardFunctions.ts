7e783480cfd3eb989ed4ea101432ca334c0e1915
Sean Fong <sean.fong@csiro.au>
2023-02-23 22:46:54 +1030

    Create dashboard functions file

---
+++ apps/smart-forms-react-app/src/functions/DashboardFunctions.ts
@@ -0,0 +1,134 @@
+// ----------------------------------------------------------------------
+
+import { filter } from 'lodash';
+import {
+  ListItem,
+  ListItemWithIndex,
+  QuestionnaireListItem,
+  ResponseListItem
+} from '../interfaces/Interfaces';
+import * as FHIR from 'fhirclient';
+import { Bundle, Questionnaire, QuestionnaireResponse } from 'fhir/r5';
+import randomColor from 'randomcolor';
+import dayjs from 'dayjs';
+import { getQuestionnaireNameFromResponse } from './ItemControlFunctions';
+
+export function descendingComparator(
+  a: ListItem,
+  b: ListItem,
+  orderBy: keyof QuestionnaireListItem | keyof ResponseListItem,
+  listType: 'questionnaire' | 'response'
+): number {
+  if (listType === 'questionnaire') {
+    orderBy = orderBy as keyof QuestionnaireListItem;
+    a = a as QuestionnaireListItem;
+    b = b as QuestionnaireListItem;
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+  } else {
+    orderBy = orderBy as keyof ResponseListItem;
+    a = a as ResponseListItem;
+    b = b as ResponseListItem;
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+export function getComparator(
+  order: 'asc' | 'desc',
+  orderBy: keyof QuestionnaireListItem | keyof ResponseListItem,
+  listType: 'questionnaire' | 'response'
+): (a: ListItem, b: ListItem) => number {
+  return order === 'desc'
+    ? (a: ListItem, b: ListItem) => descendingComparator(a, b, orderBy, listType)
+    : (a: ListItem, b: ListItem) => -descendingComparator(a, b, orderBy, listType);
+}
+
+export function applySortFilter(
+  array: ListItem[],
+  comparator: (a: ListItem, b: ListItem) => number,
+  query: string
+): ListItem[] {
+  const stabilizedThis: ListItemWithIndex[] = array.map((el, index) => [index, el]);
+
+  stabilizedThis.sort((a: ListItemWithIndex, b: ListItemWithIndex) => {
+    const order = comparator(a[1], b[1]);
+    if (order !== 0) {
+      return order;
+    }
+
+    return a[0] - b[0];
+  });
+
+  if (query) {
+    return filter(array, (_user) => _user.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+
+  return stabilizedThis.map((el) => el[1]);
+}
+
+export function getBundlePromise(endpointUrl: string, queryUrl: string): Promise<Bundle> {
+  return FHIR.client({ serverUrl: endpointUrl }).request({
+    url: queryUrl
+  });
+}
+
+export function getQuestionnaireListItems(bundle: Bundle | undefined): QuestionnaireListItem[] {
+  if (!bundle || !bundle.entry || bundle.entry.length === 0) return [];
+
+  return bundle.entry
+    .filter((entry) => entry.resource && entry.resource.resourceType === 'Questionnaire')
+    .map((entry, i) => {
+      const questionnaire = entry.resource as Questionnaire; // non-questionnaire resources are filtered
+      const questionnaireTitle = questionnaire.title ?? 'Untitled';
+
+      const questionnaireListItem: QuestionnaireListItem = {
+        id: questionnaire.id ?? i.toString(),
+        title: questionnaireTitle,
+        avatarColor: randomColor({ luminosity: 'dark', seed: questionnaireTitle }),
+        publisher: questionnaire.publisher ?? '—',
+        date: questionnaire.date ? dayjs(questionnaire.date).format('LL') : '—',
+        status: questionnaire.status
+      };
+      return questionnaireListItem;
+    });
+}
+
+export function getResponseListItems(bundle: Bundle | undefined): ResponseListItem[] {
+  if (!bundle || !bundle.entry || bundle.entry.length === 0) return [];
+
+  return bundle.entry
+    .filter((entry) => entry.resource && entry.resource.resourceType === 'QuestionnaireResponse')
+    .map((entry, i) => {
+      const response = entry.resource as QuestionnaireResponse; // non-questionnaire resources are filtered
+      const responseTitle = getQuestionnaireNameFromResponse(response);
+
+      const responseListItem: ResponseListItem = {
+        id: response.id ?? i.toString(),
+        title: responseTitle,
+        avatarColor: randomColor({ luminosity: 'dark', seed: responseTitle }),
+        author: response.author?.display ?? '—',
+        authored: response.authored ?? '—',
+        status: response.status
+      };
+      return responseListItem;
+    });
+}
+
+export function getResponsesFromBundle(bundle: Bundle | undefined): QuestionnaireResponse[] {
+  if (!bundle || !bundle.entry || bundle.entry.length === 0) return [];
+
+  return bundle.entry
+    .filter((entry) => entry.resource && entry.resource.resourceType === 'QuestionnaireResponse')
+    .map((entry) => entry.resource as QuestionnaireResponse); // non-questionnaire response resources are filtered
+}
