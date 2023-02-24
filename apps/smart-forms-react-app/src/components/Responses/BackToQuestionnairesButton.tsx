d0b62cefe7af684a1740f55219aa90fe6487ed5d
Sean Fong <sean.fong@csiro.au>
2023-02-24 11:31:35 +1030

    Create back to questionnaires button

---
+++ apps/smart-forms-react-app/src/components/Responses/BackToQuestionnairesButton.tsx
@@ -0,0 +1,30 @@
+// @mui
+import { Button } from '@mui/material';
+import React, { useContext } from 'react';
+import Iconify from '../Iconify';
+import { SelectedQuestionnaireContext } from '../../custom-contexts/SelectedQuestionnaireContext';
+import { useNavigate } from 'react-router-dom';
+
+function BackToQuestionnairesButton() {
+  const { existingResponses } = useContext(SelectedQuestionnaireContext);
+  const navigate = useNavigate();
+
+  return existingResponses.length > 0 ? (
+    <Button
+      variant="contained"
+      endIcon={<Iconify icon="material-symbols:arrow-back" />}
+      sx={{
+        backgroundColor: 'primary.main',
+        '&:hover': {
+          backgroundColor: 'primary.dark'
+        }
+      }}
+      onClick={() => {
+        navigate('/questionnaires');
+      }}>
+      Go back
+    </Button>
+  ) : null;
+}
+
+export default BackToQuestionnairesButton;
