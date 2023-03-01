a0bc11c5b68676d13dfc7d1c791a741b52681e45
Sean Fong <sean.fong@csiro.au>
2023-03-01 14:11:15 +1030

    Fix redirect to renderer on first load

--- apps/smart-forms-react-app/src/components/Dashboard/DashboardLayout.tsx
+++ apps/smart-forms-react-app/src/components/Dashboard/DashboardLayout.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DashboardHeader from './DashboardHeader/DashboardHeader';
 import DashboardNav from './DashboardNav/DashboardNav';
 import { Main, StyledRoot } from '../StyledComponents/Layout.styles';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import SelectedQuestionnaireContextProvider from '../../custom-contexts/SelectedQuestionnaireContext';
 
 function DashboardLayout() {
   const [open, setOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Get initial questionnaire and redirect to renderer
+    const questionnaireUrl = sessionStorage.getItem('questionnaireUrl');
+    if (questionnaireUrl) {
+      navigate('/renderer');
+      sessionStorage.removeItem('questionnaireUrl');
+    }
+  }, []); // redirect to renderer on first render, leave dependency array empty
 
   return (
     <StyledRoot>
