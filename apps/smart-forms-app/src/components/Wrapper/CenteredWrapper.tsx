7bc7a009fb019e7677962361fa53d95055736fcc
Sean Fong <sean.fong@csiro.au>
2023-07-20 14:49:23 +0930

    Create error handling for invalid launch params and tidy up spinner wrappers

---
+++ apps/smart-forms-app/src/components/Wrapper/CenteredWrapper.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Commonwealth Scientific and Industrial Research
+ * Organisation (CSIRO) ABN 41 687 119 230.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ReactNode } from 'react';
+import { Container, Stack } from '@mui/material';
+
+interface CenteredWrapperProps {
+  children: ReactNode;
+}
+
+function CenteredWrapper(props: CenteredWrapperProps) {
+  const { children } = props;
+
+  return (
+    <Stack justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
+      <Container>{children}</Container>
+    </Stack>
+  );
+}
+
+export default CenteredWrapper;
---
+++ apps/smart-forms-app/src/features/smartAppLaunch/components/LaunchView.tsx
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Commonwealth Scientific and Industrial Research
+ * Organisation (CSIRO) ABN 41 687 119 230.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import ProgressSpinner from '../../../components/Spinners/ProgressSpinner.tsx';
+import type { LaunchState } from './Launch.tsx';
+import { useNavigate } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
+import CenteredWrapper from '../../../components/Wrapper/CenteredWrapper.tsx';
+
+interface LaunchViewProps {
+  launchState: LaunchState;
+}
+
+function LaunchView(props: LaunchViewProps) {
+  const { launchState } = props;
+
+  const navigate = useNavigate();
+
+  if (launchState === 'error') {
+    return (
+      <CenteredWrapper>
+        <Stack rowGap={2}>
+          <Typography variant="h3">An error occurred while launching the app.</Typography>
+          <Typography fontSize={13}>
+            {"You might have incorrect launch parameters in the URL or the server doesn't exist. "}
+            <br />
+            {'Please contact your administrator for assistance.'}
+          </Typography>
+        </Stack>
+        <Button
+          variant="contained"
+          color="warning"
+          sx={{ mt: 6 }}
+          onClick={() => {
+            navigate('/dashboard/questionnaires');
+          }}>
+          Proceed in unlaunched state
+        </Button>
+      </CenteredWrapper>
+    );
+  }
+
+  return <ProgressSpinner message="Launching Smart Forms" />;
+}
+
+export default LaunchView;
