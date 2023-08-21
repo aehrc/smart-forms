4731af997568a3358dfb8e2f24d01dd676883ea5
Sean Fong <sean.fong@csiro.au>
2023-08-21 15:13:39 +1000

    Setup storybook

---
+++ apps/smart-forms-app/.storybook/preview.ts
@@ -0,0 +1,34 @@
+import type { Preview } from '@storybook/react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { createTheme } from '@mui/material/styles';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: createTheme()
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline
+  })
+];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/
+      }
+    }
+  }
+};
+
+export default preview;
---
+++ apps/smart-forms-app/src/stories/TextItem.stories.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023 Commonwealth Scientific and Industrial Research
+ * Organisation (CSIRO) ABN 41 687 119 230.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TextItem from '../features/renderer/components/FormPage/QFormComponents/TextItem/TextItem.tsx';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'FormComponent/TextItem',
+  component: TextItem,
+  argTypes: {
+    onQrItemChange: { action: 'change' }
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs']
+} satisfies Meta<typeof TextItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const ItemWithoutAnswer: Story = {
+  args: {
+    qItem: {
+      linkId: '17596726-34cf-4133-9960-7081e1d63558',
+      text: 'Name',
+      type: 'string',
+      repeats: false
+    },
+    qrItem: {
+      linkId: '17596726-34cf-4133-9960-7081e1d63558',
+      text: 'Name'
+    },
+    isRepeated: false
+  }
+};
+
+export const ItemWithAnswer: Story = {
+  args: {
+    qItem: {
+      linkId: '17596726-34cf-4133-9960-7081e1d63558',
+      text: 'Name',
+      type: 'string',
+      repeats: false
+    },
+    qrItem: {
+      linkId: '17596726-34cf-4133-9960-7081e1d63558',
+      text: 'Name',
+      answer: [
+        {
+          valueString: 'John'
+        }
+      ]
+    },
+    isRepeated: false
+  }
+};
