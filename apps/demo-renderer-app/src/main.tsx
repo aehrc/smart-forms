ba023cd313cb5020a97f2d70fe9c061a76e47489
Sean Fong <sean.fong@csiro.au>
2023-11-24 08:32:41 +1030

    Add url input box to demo app

--- apps/demo-renderer-app/src/main.tsx
+++ apps/demo-renderer-app/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { DefaultOptions, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const DEFAULT_QUERY_OPTIONS: DefaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+    keepPreviousData: true
+  }
+};
+
+const queryClient = new QueryClient({ defaultOptions: DEFAULT_QUERY_OPTIONS });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
