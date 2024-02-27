b2d18f00876a82dea1786bbd7231d098de7c41e4
Sean Fong <sean.fong@csiro.au>
2024-02-27 14:09:37 +1030

    Add partially working initial expression logic

--- packages/structure-def-to-questionnaire/structure-def-to-questionnaire.py
+++ packages/structure-def-to-questionnaire/structure-def-to-questionnaire.py
@@ -1,7 +1,13 @@
 import json
 from datetime import date
 import platform
-from transformDatatypes import create_item, create_address_item
+from transformDatatypes import (
+    create_item,
+    create_address_item,
+    create_period_item,
+    create_human_name_item,
+    create_contact_point_item,
+)
 
 from bs4 import BeautifulSoup
 
@@ -23,6 +29,8 @@ else:
     OK_GREEN = "\033[92m"
     INFO_BLUE = "\033[94m"
     END_C = "\033[0m"
+
+profile_resource_type = None
 
 
 def get_item_type(type_code, type_extension=None):
@@ -61,17 +69,20 @@ def get_item_type(type_code, type_extension=None):
     if type_code == "date":
         return "date"
 
-    # item.type=date
-    if type_code == "date":
-        return "datetime"
+    # item.type=dateTime
+    if type_code == "dateTime":
+        return "dateTime"
 
     # type=code or type=CodeableConcept
-    if type_code == "code" or type_code == "CodeableConcept":
+    if type_code == "code" or type_code == "CodeableConcept" or type_code == "Coding":
         return "choice"
 
     # type=Attachment
     if type_code == "Attachment":
         return "attachment"
+
+    if type_code == "Reference":
+        return "reference"
 
     # Complex type
     if (
@@ -83,7 +94,6 @@ def get_item_type(type_code, type_extension=None):
         or type_code == "Address"
         or type_code == "BackboneElement"
         or type_code == "ContactPoint"
-        or type_code == "Reference"
         or type_code == "Period"
         or type_code == "Identifier"
     ):
@@ -104,7 +114,14 @@ def slot_item_into_qitems(element, item, q_items, previous_element):
         return q_items
 
     # Find the parent item in q_items and append the item to it
-    parent_item_id = element["id"].split(":")[0]
+    parent_item_id = ""
+    if ":" in element["id"]:
+        parent_item_id = element["id"].split(":")[0]
+    elif "." in element["id"]:
+        parent_item_id = element["id"].split(".")[:-1]
+
+    print(parent_item_id)
+    print()
     for q_item in q_items:
         if q_item["linkId"] == parent_item_id:
             q_item["item"].append(item)
@@ -120,36 +137,47 @@ def slot_item_into_qitems(element, item, q_items, previous_element):
     return q_items
 
 
-# def resolve_profile(profile_string):
-#     try:
-#         response = requests.get(profile_string, headers=header)
-#         print(response)
-#         print(response.status_code, response.headers["location"])
-#         response.raise_for_status()
-#         # print(response.json())
+def get_element_name_without_resource(element, root_level=False):
+    if root_level:
+        return ".".join(element["id"].split(".")[1:])
 
-#         # Check the response status code
-#         if str(response.status_code) == "200":
-#             # Extract the bundle from the response
-#             print(response.json())
+    if ":" not in element["id"]:
+        return element["id"]
 
-#             # Append the bundle to the list of all bundles
-#             # all_bundles.append(bundle)
-#             print(f"{OK_GREEN}{END_C}")
-#         else:
-#             print(f"{ERROR_RED}{END_C}")
-
-#     except requests.exceptions.HTTPError as e:
-#         print(f"{ERROR_RED}ERROR: HTTP ERROR THROWN:{END_C}", e, "\n")
-#     except Exception as e:
-#         print(f"{ERROR_RED}ERROR: An error occurred, details:", e, "\n")
+    # if element is not at root level (i.e. it was further obtained via profile resolution), only use the last part of the id
+    print(element["id"])
+    return element["id"].split(":")[1]
 
 
-def get_element_name_without_resource(element):
-    return ".".join(element["id"].split(".")[1:])
+def get_profile_link(profile_links, element_name, root_level=False):
+    if root_level:
+        return profile_links[0]
+
+    # not root level, resolve via appending profie name to end of base url (hardcoded)
+    temp_link = None
+    for link in profile_links:
+        if "extension" in link:
+            return link
+
+        #     return "https://build.fhir.org/ig/hl7au/au-fhir-base/e"
+        if "StructureDefinition" in link:
+            print(link)
+            try:
+                # Split the URL by "StructureDefinition-" and ".html"
+                part = link.split("StructureDefinition-")[1].split(".html")[0]
+                return (
+                    "https://build.fhir.org/ig/hl7au/au-fhir-base/StructureDefinition-"
+                    + part
+                    + ".json"
+                )
+            except IndexError:
+                print("Fail to get profile link ", link)
+                return None
+
+    return temp_link
 
 
-def get_profile_definition(profile_link, encoding="utf-8"):
+def get_profile_definition(profile_link):
     if ".html" in profile_link:
         profile_link = profile_link.replace(".html", ".json")
 
@@ -178,67 +206,61 @@ def get_profile_definition(profile_link, encoding="utf-8"):
     return None
 
 
-def process_complex_types(item, element, profile_elements):
+def process_item_by_code(item, element):
+    if "code" in element["type"][0]:
+        code = element["type"][0]["code"]
+
+        if element["id"] == "communication":
+
+
+        if code == "Address":
+            return create_address_item(element, item)
+
+        if code == "Period":
+            return create_period_item(element, item)
+
+        if code == "HumanName":
+            return create_human_name_item(element, item)
+
+        if code == "ContactPoint":
+            return create_contact_point_item(element, item)
+
+    return item
+
+
+def process_complex_types(item, element, table_elements, root_level=False):
     item["item"] = []
 
     if "profile" in element["type"][0]:
-        element_name = get_element_name_without_resource(element)
+        element_name = get_element_name_without_resource(element, root_level)
 
-        if element_name not in profile_elements:
+        if element_name not in table_elements:
             return item
 
-        element_profile = profile_elements[element_name]
+        element_profile = table_elements[element_name]
+
         profile_links = [
             link for link in list(element_profile.values()) if link.startswith("http")
         ]
 
         # Get the first profile link
-        profile_link = profile_links[0]
-
-        # TODO work on visiting the profile link then extract the structure and form it into a questionnaire item
-        # TODO can potentially use a recursive
+        profile_link = get_profile_link(profile_links, element_name)
 
         profile_definition = get_profile_definition(profile_link)
-
         if profile_definition is None:
             return item
 
-        item["item"] = process_profile_elements(profile_definition, profile_elements)
+        # get profile elements
+        child_table_elements = {}
+        if profile_definition.get("text", {}).get("div") is not None:
+            child_table_elements = get_table_elements(profile_definition["text"]["div"])
 
-        print()
+        item["item"] = process_elements(profile_definition, child_table_elements)
+
         return item
 
-    if "code" in element["type"][0]:
-        if element["type"][0]["code"] == "Address":
-            return create_address_item(element, item)
-
-    return item
-
-
-def process_profile_elements(
-    profile_definition, profile_elements, mustSupportFlag=False
-):
-    q_items = []
-    previous_element = None
-    for element in profile_definition["snapshot"]["element"]:
-
-        # skip elements without an id or type
-        if element.get("id") is None or element.get("type") is None:
-            previous_element = element
-            continue
-
-        # if mustSupportFlag is True, skip non-must have elements
-        if mustSupportFlag and element.get("mustSupport", False) is False:
-            previous_element = element
-            continue
-
-        item = process_profile_element(element, profile_elements)
-
-        # determine path to place item
-        q_items = slot_item_into_qitems(element, item, q_items, previous_element)
-        previous_element = element
-
-    return q_items
+    # Not a profile, process "code"
+    return process_item_by_code(item, element)
 
 
 def create_item(element):
@@ -261,7 +283,57 @@ def create_item(element):
     return item
 
 
-def process_profile_element(element, profile_elements):
+def add_initial_expression_with_profile(item, element):
+    if item.get("extension") is None:
+        item["extension"] = []
+
+    initialExpression = {
+        "url": "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-initialExpression",
+        "valueExpression": {
+            "language": "text/fhirpath",
+            "expression": f"%{element.get('path').lower()}",
+        },
+    }
+
+    item["extension"].append(initialExpression)
+
+    return item
+
+
+def add_initial_expression(item, element):
+    if item.get("extension") is None:
+        item["extension"] = []
+
+    if "profile" in element["type"][0]:
+        initialExpression = {
+            "url": "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-initialExpression",
+            "valueExpression": {
+                "language": "text/fhirpath",
+                "expression": f"%{element['path'].lower()}.where(url='{element['type'][0]['profile'][0]}').value",
+            },
+        }
+        print("initialExpression 1st")
+        print(initialExpression)
+        print()
+    else:
+        initialExpression = {
+            "url": "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-initialExpression",
+            "valueExpression": {
+                "language": "text/fhirpath",
+                "expression": f"%{element.get('path').lower()}",
+            },
+        }
+
+        print("initialExpression 2nd")
+        print(initialExpression)
+        print()
+
+    item["extension"].append(initialExpression)
+
+    return item
+
+
+def process_element(element, table_elements, root_level=False):
     item = create_item(element)
 
     # multiple types
@@ -277,9 +349,17 @@ def process_profile_element(element, profile_elements):
         item_type = get_item_type(type_code)
         item["type"] = item_type
 
+        # Rename ids ending with value[x]
+        if "value[x]" in element["id"]:
+            modified_id = element["id"].replace(
+                "value[x]", "value" + item_type.capitalize()
+            )
+            item["linkId"] = modified_id
+            item["text"] = modified_id
+
         # Complex type
         if item_type == "group":
-            item = process_complex_types(item, element, profile_elements)
+            item = process_complex_types(item, element, table_elements, root_level)
 
         # convert id to string
         if item_type == "id":
@@ -291,10 +371,37 @@ def process_profile_element(element, profile_elements):
             if "valueSet" in element.get("binding", {}):
                 item["answerValueSet"] = element["binding"]["valueSet"]
 
+        # add initialExpression
+        item = add_initial_expression(item, element)
+
     return item
 
 
-def create_questionnaire(structure_definition, profile_elements):
+def process_elements(profile_definition, table_elements, root_level=False):
+    q_items = []
+    previous_element = None
+    for element in profile_definition["snapshot"]["element"]:
+
+        # skip elements without an id or type
+        if element.get("id") is None or element.get("type") is None:
+            previous_element = element
+            continue
+
+        # if mustSupportFlag is True, skip non-must have elements
+        if root_level and element.get("mustSupport", False) is False:
+            previous_element = element
+            continue
+
+        item = process_element(element, table_elements, root_level)
+
+        # determine path to place item
+        q_items = slot_item_into_qitems(element, item, q_items, previous_element)
+        previous_element = element
+
+    return q_items
+
+
+def create_questionnaire(structure_definition, table_elements):
     resource_type = structure_definition.get("resourceType", None)
     if resource_type != "StructureDefinition":
         return None
@@ -307,9 +414,31 @@ def create_questionnaire(structure_definition, profile_elements):
         "item": [],
     }
 
-    questionnaire["item"] = process_profile_elements(
-        structure_definition, profile_elements, mustSupportFlag=True
+    questionnaire["item"] = process_elements(
+        structure_definition, table_elements, root_level=True
     )
+
+    # add LaunchContext
+    profile_resource_type = structure_definition["type"]
+    questionnaire["extension"] = [
+        {
+            "url": "http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-launchContext",
+            "extension": [
+                {
+                    "url": "name",
+                    "valueCoding": {
+                        "system": "http://hl7.org/fhir/uv/sdc/CodeSystem/launchContext",
+                        "code": profile_resource_type.lower(),
+                    },
+                },
+                {"url": "type", "valueCode": profile_resource_type},
+                {
+                    "url": "description",
+                    "valueString": f"The {profile_resource_type.lower()} that is to be used to pre-populate the form",
+                },
+            ],
+        }
+    ]
 
     return questionnaire
 
@@ -318,13 +447,13 @@ def html_string_to_html(html_string):
     return BeautifulSoup(html_string, "html.parser")
 
 
-def get_profile_elements_from_table(text_div):
+def get_table_elements(text_div):
     # Find all table rows, skipping the header row and final row
     soup = BeautifulSoup(text_div, "html.parser")
     rows = soup.find_all("tr")[1:-1]
 
     # Create a list of dictionaries, each containing the element name and a dictionary of element names and URLs
-    profile_elements = {}
+    table_elements = {}
     previous_element = ""
     for row in rows:
         # Find all <a> tags within the row
@@ -351,31 +480,30 @@ def get_profile_elements_from_table(text_div):
 
         # Add profile element
         if not all(key[0].isupper() for key in a_tags_dict.keys()):
-            profile_elements[a_tag_element_name] = a_tags_dict
+            table_elements[a_tag_element_name] = a_tags_dict
 
-    return profile_elements
+    return table_elements
 
 
 if __name__ == "__main__":
     # Replace 'your_directory_path' with the path to the directory containing JSON files
-    struc_def_file = "./input/StructureDefinition-au-core-patient.json"
+    structure_definition_file = "./input/StructureDefinition-au-core-patient.json"
 
-    with open(struc_def_file, "r") as file:
-        struc_def_json = json.load(file)
+    with open(structure_definition_file, "r") as file:
+        structure_definition = json.load(file)
 
-        if struc_def_json.get("text", {}).get("div") is not None:
-            profile_elements = get_profile_elements_from_table(
-                struc_def_json["text"]["div"]
-            )
+        profile_elements = {}
+        if structure_definition.get("text", {}).get("div") is not None:
+            table_elements = get_table_elements(structure_definition["text"]["div"])
 
-            for item in profile_elements:
-                print(item, profile_elements[item])
-            print(len(profile_elements))
+        for el in table_elements:
+            print(el, table_elements[el])
+        print("_______________________________")
 
-            questionnaire = create_questionnaire(struc_def_json, profile_elements)
+        questionnaire = create_questionnaire(structure_definition, table_elements)
 
-            with open("./output/generated_questionnaire.json", "w") as file:
-                json.dump(questionnaire, file, indent=2)
+        with open("./output/generated_questionnaire.json", "w") as file:
+            json.dump(questionnaire, file, indent=2)
 
     #     questionnaire = create_questionnaire(struc_def_json)
 
--- packages/structure-def-to-questionnaire/transformDatatypes.py
+++ packages/structure-def-to-questionnaire/transformDatatypes.py
@@ -19,82 +19,222 @@ def create_item(element):
 
 
 def create_address_item(element, item):
-    item["type"] = "group"
-
-    item["item"] = [
-        {
-            "linkId": f"{element['id']}.use",
-            "text": "Address.use",
-            "type": "choice",
-            "required": "false",
-            "repeats": "false",
-            "answerValueSet": "http://hl7.org/fhir/ValueSet/address-use|4.0.1",
-        },
-        {
-            "linkId": f"{element['id']}.type",
-            "text": "Address.type",
-            "type": "choice",
-            "required": False,
-            "repeats": False,
-            "answerValueSet": "http://hl7.org/fhir/ValueSet/address-type|4.0.1",
-        },
-        {
-            "linkId": f"{element['id']}.text",
-            "text": "Address.text",
-            "type": "string",
-            "required": False,
-            "repeats": False,
-        },
-        {
-            "linkId": f"{element['id']}.line",
-            "text": "Address.line",
-            "type": "string",
-            "required": False,
-            "repeats": True,
-        },
-        {
-            "linkId": f"{element['id']}.city",
-            "text": "Address.city",
-            "type": "string",
-            "required": False,
-            "repeats": False,
-        },
-        {
-            "linkId": f"{element['id']}.district",
-            "text": "Address.district",
-            "type": "string",
-            "required": False,
-            "repeats": False,
-        },
-        {
-            "linkId": f"{element['id']}.state",
-            "text": "Address.state",
-            "type": "string",
-            "required": False,
-            "repeats": False,
-        },
-        {
-            "linkId": f"{element['id']}.postalCode",
-            "text": "Address.postalCode",
-            "type": "string",
-            "required": False,
-            "repeats": False,
-        },
-        {
-            "linkId": f"{element['id']}.country",
-            "text": "Address.country",
-            "type": "string",
-            "required": False,
-            "repeats": False,
-        },
-        {
+    if item["type"] == "group":
+        period_item = {
             "linkId": f"{element['id']}.period",
             "text": "Address.period",
             "type": "group",
             "required": False,
             "repeats": False,
-            "item": [],
-        },
-    ]
+        }
+        period_item = create_period_item(element, period_item)
+
+        item["item"] = [
+            {
+                "linkId": f"{element['id']}.use",
+                "text": "Address.use",
+                "type": "choice",
+                "required": False,
+                "repeats": False,
+                "answerValueSet": "http://hl7.org/fhir/ValueSet/address-use|4.0.1",
+            },
+            {
+                "linkId": f"{element['id']}.type",
+                "text": "Address.type",
+                "type": "choice",
+                "required": False,
+                "repeats": False,
+                "answerValueSet": "http://hl7.org/fhir/ValueSet/address-type|4.0.1",
+            },
+            {
+                "linkId": f"{element['id']}.text",
+                "text": "Address.text",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.line",
+                "text": "Address.line",
+                "type": "string",
+                "required": False,
+                "repeats": True,
+            },
+            {
+                "linkId": f"{element['id']}.city",
+                "text": "Address.city",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.district",
+                "text": "Address.district",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.state",
+                "text": "Address.state",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.postalCode",
+                "text": "Address.postalCode",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.country",
+                "text": "Address.country",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            period_item,
+        ]
+
+    return item
+
+
+def create_period_item(element, item):
+    if item["type"] == "group":
+        item["item"] = [
+            {
+                "linkId": f"{element['id']}.start",
+                "text": "Period.start",
+                "type": "dateTime",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.end",
+                "text": "Period.end",
+                "type": "dateTime",
+                "required": False,
+                "repeats": False,
+            },
+        ]
+
+    return item
+
+
+def create_human_name_item(element, item):
+    if item["type"] == "group":
+
+        period_item = {
+            "linkId": f"{element['id']}.period",
+            "text": "HumanName.period",
+            "type": "group",
+            "required": False,
+            "repeats": False,
+        }
+        period_item = create_period_item(element, period_item)
+
+        item["item"] = [
+            {
+                "linkId": f"{element['id']}.use",
+                "text": "HumanName.use",
+                "type": "choice",
+                "required": False,
+                "repeats": False,
+                "answerValueSet": "http://hl7.org/fhir/ValueSet/name-use|4.0.1",
+            },
+            {
+                "linkId": f"{element['id']}.text",
+                "text": "HumanName.text",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.family",
+                "text": "HumanName.family",
+                "type": "string",
+                "required": False,
+                "repeats": True,
+            },
+            {
+                "linkId": f"{element['id']}.given",
+                "text": "HumanName.given",
+                "type": "string",
+                "required": False,
+                "repeats": True,
+            },
+            {
+                "linkId": f"{element['id']}.prefix",
+                "text": "HumanName.prefix",
+                "type": "string",
+                "required": False,
+                "repeats": True,
+            },
+            {
+                "linkId": f"{element['id']}.suffix",
+                "text": "HumanName.suffix",
+                "type": "string",
+                "required": False,
+                "repeats": True,
+            },
+            period_item,
+        ]
+
+    return item
+
+
+def create_contact_point_item(element, item):
+    if item["type"] == "group":
+
+        period_item = {
+            "linkId": f"{element['id']}.period",
+            "text": "ContactPoint.period",
+            "type": "group",
+            "required": False,
+            "repeats": False,
+        }
+        period_item = create_period_item(element, period_item)
+
+        item["item"] = [
+            {
+                "linkId": f"{element['id']}.system",
+                "text": "ContactPoint.system",
+                "type": "choice",
+                "required": False,
+                "repeats": False,
+                "answerValueSet": "http://hl7.org/fhir/ValueSet/contact-point-system|4.0.1",
+            },
+            {
+                "linkId": f"{element['id']}.value",
+                "text": "ContactPoint.value",
+                "type": "string",
+                "required": False,
+                "repeats": False,
+            },
+            {
+                "linkId": f"{element['id']}.use",
+                "text": "ContactPoint.use",
+                "type": "choice",
+                "required": False,
+                "repeats": False,
+                "answerValueSet": "http://hl7.org/fhir/ValueSet/contact-point-use|4.0.1",
+            },
+            {
+                "linkId": f"{element['id']}.rank",
+                "text": "ContactPoint.rank",
+                "type": "integer",
+                "required": False,
+                "repeats": False,
+                "extension": [
+                    {
+                        "url": "http://hl7.org/fhir/StructureDefinition/minValue",
+                        "valueInteger": 1,
+                    }
+                ],
+            },
+            period_item,
+        ]
 
     return item
