d895487cbb1ade362eb0ee1d488f589c63c1d98c
Sean Fong <sean.fong@csiro.au>
2024-05-08 20:17:09 +0930

    Add advanced control appearance, itemControl group and itemControl Display stories

--- packages/smart-forms-renderer/src/stories/ItemControlGroup.stories.tsx
+++ packages/smart-forms-renderer/src/stories/ItemControlGroup.stories.tsx
@@ -15,17 +15,34 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2024 Commonwealth Scientific and Industrial Research
+ * Organisation (CSIRO) ABN 41 687 119 230.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import type { Meta, StoryObj } from '@storybook/react';
 import BuildFormWrapper from './BuildFormWrapper';
 import {
-  qChoiceOrientation,
-  qItemControl,
-  qSliderStepValue
-} from './assets/QAdvancedControlAppearance';
+  qItemControlDisplayTabContainer,
+  qItemControlGroupGrid,
+  qItemControlGroupGTable
+} from './assets/QItemControlGroup';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Component/Advanced Control Appearance',
+  title: 'Component/Advanced Control Appearance - Item Control Group',
   component: BuildFormWrapper,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: []
@@ -36,20 +53,20 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 
-export const ItemControl: Story = {
+export const Gtable: Story = {
   args: {
-    questionnaire: qItemControl
+    questionnaire: qItemControlGroupGTable
   }
 };
 
-export const ChoiceOrientation: Story = {
+export const Grid: Story = {
   args: {
-    questionnaire: qChoiceOrientation
+    questionnaire: qItemControlGroupGrid
   }
 };
 
-export const SliderStepValue: Story = {
+export const TabContainer: Story = {
   args: {
-    questionnaire: qSliderStepValue
+    questionnaire: qItemControlDisplayTabContainer
   }
 };
