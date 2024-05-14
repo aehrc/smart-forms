c3b5d2120c482668bf65d38149321e534324c9dd
Sean Fong <sean.fong@csiro.au>
2024-05-14 12:10:51 +0930

    Test github pages deployment #6

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://aehrc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/smart-forms/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
