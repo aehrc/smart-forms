2264ad0c44abc394a7bdc56bbd326fd319e733f8
Sean Fong <sean.fong@csiro.au>
2024-05-23 20:03:50 +0930

    Fix broken links - one is from the FHIR spec so config is changed to only warn

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   projectName: 'smart-forms', // Usually your repo name.\
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
