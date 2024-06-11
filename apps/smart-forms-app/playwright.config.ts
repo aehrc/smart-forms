098568c59bed7730e2e553679d0554747f27c0df
Sean Fong <sean.fong@csiro.au>
2024-06-12 08:57:49 +0930

    Run playwright in parallel with 4 workers

--- apps/smart-forms-app/playwright.config.ts
+++ apps/smart-forms-app/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -71,8 +71,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build && npm run preview',
-    url: 'http://localhost:4173',
+    command: 'npm run start',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI
   }
 });
