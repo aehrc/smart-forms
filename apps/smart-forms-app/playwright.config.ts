786fd0663a5d68d71039c21d68bf764aec1cb9f8
Sean Fong <sean.fong@csiro.au>
2024-06-11 11:55:24 +0930

    Tweak playwright ci #2

--- apps/smart-forms-app/playwright.config.ts
+++ apps/smart-forms-app/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run build && npm run preview',
     url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI
   }
