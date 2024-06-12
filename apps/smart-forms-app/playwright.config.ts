68d5b4451df1f4956885c8e2e3d6ee46ec79c3c2
Sean Fong <sean.fong@csiro.au>
2024-06-12 09:52:58 +0930

    Fix playwright config

--- apps/smart-forms-app/playwright.config.ts
+++ apps/smart-forms-app/playwright.config.ts
@@ -71,8 +71,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:5173',
+    command: 'npm run build && npm run preview',
+    url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI
   }
 });
