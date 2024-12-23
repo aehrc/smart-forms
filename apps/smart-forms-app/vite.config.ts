69d18947e535e231f244ac46c99214e245d70595
Sean Fong <sean.fong@csiro.au>
2024-12-23 11:07:04 +1030

    Add VITE_PRESERVE_SYM_LINKS env to GitHub actions files

--- apps/smart-forms-app/vite.config.ts
+++ apps/smart-forms-app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-const preserveSymlinks = process.env.VITE_PRESERVE_SYM_LINKS === 'true';//resolve preserveSymlink to boolean(Explicitly parse the string to a boolean)
+const preserveSymlinks = process.env.VITE_PRESERVE_SYM_LINKS === 'true'; //resolve preserveSymlink to boolean(Explicitly parse the string to a boolean)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +17,3 @@ export default defineConfig({
   },
   resolve: { preserveSymlinks: preserveSymlinks }
 });
-
