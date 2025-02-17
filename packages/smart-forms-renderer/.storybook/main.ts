cf34d44d8f94238e63497d9ceadae6780503b5ed
Sean Fong <sean.fong@csiro.au>
2025-02-17 10:56:54 +1030

    Add renderer version on Storybook via MDX

--- packages/smart-forms-renderer/.storybook/main.ts
+++ packages/smart-forms-renderer/.storybook/main.ts
@@ -1,6 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-// @ts-ignore
-import { version } from '../package.json';
 
 import { dirname, join } from 'path';
 
@@ -12,10 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    { directory: '../src/stories', titlePrefix: `Renderer v${version}` }
-  ],
+  stories: [{ directory: '../src/stories', titlePrefix: 'CSIRO Renderer' }],
 
   addons: [
     getAbsolutePath('@storybook/addon-links'),
@@ -36,4 +31,5 @@ const config: StorybookConfig = {
     reactDocgen: 'react-docgen-typescript'
   }
 };
+
 export default config;
