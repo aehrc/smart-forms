69d2d922ed95e2d2175772219174a9b668aa00cc
Sean Fong <sean.fong@csiro.au>
2025-03-27 13:58:00 +1000

    Smart Forms Renderer - Restore extension element back to Codings - relevant to #1088

--- packages/smart-forms-renderer/src/utils/valueSet.ts
+++ packages/smart-forms-renderer/src/utils/valueSet.ts
@@ -145,13 +145,22 @@ export async function resolveValueSetPromises(
  * @author Sean Fong
  */
 export function getValueSetCodings(valueSet: ValueSet): Coding[] {
-  return (
-    valueSet.expansion?.contains?.map((coding) => ({
-      system: coding.system,
-      code: coding.code,
-      display: coding.display
-    })) ?? []
-  );
+  return valueSet.expansion?.contains?.map((coding) => getRelevantCodingProperties(coding)) ?? [];
+}
+
+/**
+ * Retrieves only the relevant properties of a Coding object.
+ * Reason: https://tx.ontoserver.csiro.au/fhir returns a Coding with designation element, which is not in the FHIR spec, causing QRs with it to fail validation.
+ *
+ * @author Sean Fong
+ */
+export function getRelevantCodingProperties(coding: Coding): Coding {
+  return {
+    system: coding.system,
+    code: coding.code,
+    display: coding.display,
+    ...(coding.extension && { extension: coding.extension })
+  };
 }
 
 /**
