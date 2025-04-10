5ea8aaf756aee2fb24b646e5a0d030144abc1dea
Sean Fong <sean.fong@csiro.au>
2025-04-10 14:41:55 +0930

    Migrate demo app react-query structure from v4 to v5

--- apps/demo-renderer-app/src/main.tsx
+++ apps/demo-renderer-app/src/main.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import type { DefaultOptions } from '@tanstack/react-query';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  DefaultOptions,
+  keepPreviousData,
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
 import '@/styles/globals.css';
 
 const DEFAULT_QUERY_OPTIONS: DefaultOptions = {
   queries: {
     refetchOnWindowFocus: false,
-    keepPreviousData: true
+    placeholderData: keepPreviousData
   }
 };
 
