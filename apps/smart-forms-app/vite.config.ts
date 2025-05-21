df57581804d9a0cfc593215a185ce23740ed5641
Jörn Guy Süß <joernguy.suess@csiro.au>
2025-05-21 11:37:27 +1000

    Container build
    * Settings for building a docker container w/ nginx
    * Github actions workflow to execute the build

--- apps/smart-forms-app/vite.config.ts
+++ apps/smart-forms-app/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+// @ts-ignore
+import packageJson from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  optimizeDeps: {
-    include: ['@aehrc/sdc-assemble', '@aehrc/sdc-populate']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/, '@aehrc/sdc-assemble', '@aehrc/sdc-populate']
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react(), svgr()],
+    define: {
+      RENDERER_VERSION: JSON.stringify(packageJson.dependencies['@aehrc/smart-forms-renderer'])
     }
-  },
-  resolve: { preserveSymlinks: true }
-});
+  });
+};
