ed522629ca98e890d8f71002e65f350c870601e1
Sean Fong <sean.fong@csiro.au>
2025-06-26 13:30:15 +0930

    Add toggle in Playground to toggle item.readOnly=true HTML attribute "displayed" or "readonly"

--- apps/smart-forms-app/src/features/playground/components/PlaygroundCustomisationToggles.tsx
+++ apps/smart-forms-app/src/features/playground/components/PlaygroundCustomisationToggles.tsx
@@ -2,17 +2,36 @@ import { useQuestionnaireStore, useRendererStylingStore } from '@aehrc/smart-for
 import ToggleButton from '@mui/material/ToggleButton';
 import DisabledVisibleIcon from '@mui/icons-material/DisabledVisible';
 import SquareFootIcon from '@mui/icons-material/SquareFoot';
+import EditOffIcon from '@mui/icons-material/EditOff';
 import { Tooltip } from '@mui/material';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 function PlaygroundCustomisationToggles() {
   const enableWhenIsActivated = useQuestionnaireStore.use.enableWhenIsActivated();
   const toggleEnableWhenActivation = useQuestionnaireStore.use.toggleEnableWhenActivation();
+
+  const readOnlyVisualStyle = useRendererStylingStore.use.readOnlyVisualStyle();
   const quantityComparatorFieldHidden = useRendererStylingStore.use.hideQuantityComparatorField();
   const setRendererStyling = useRendererStylingStore.use.setRendererStyling();
 
   return (
     <ToggleButtonGroup aria-label="Renderer customisation toggles">
+      <Tooltip
+        title={`Toggle item.readOnly=true items as HTML attribute 'disabled' or 'readonly'. Current setting: ${readOnlyVisualStyle}`}>
+        <ToggleButton
+          value={readOnlyVisualStyle === 'disabled'}
+          color="primary"
+          size="small"
+          selected={readOnlyVisualStyle === 'disabled'}
+          sx={{ height: 32 }}
+          onChange={() =>
+            setRendererStyling({
+              readOnlyVisualStyle: readOnlyVisualStyle === 'disabled' ? 'readonly' : 'disabled'
+            })
+          }>
+          <EditOffIcon />
+        </ToggleButton>
+      </Tooltip>
       <Tooltip title="Show quantity comparator field">
         <ToggleButton
           value={quantityComparatorFieldHidden}
