57770f53482a61d73876875dd80bc46fd2cdeacb
Sean Fong <sean.fong@csiro.au>
2025-08-05 16:39:31 +0930

    Add Jest coverage threshold and upload coverage report in CI for sdc-template-extract

--- packages/sdc-template-extract/jest.config.ts
+++ packages/sdc-template-extract/jest.config.ts
@@ -35,6 +35,14 @@ const config: Config = {
     '!**/index.{ts,tsx}',            // exclude barrel files (e.g., index.ts)
     '!**/tests/**',                  // exclude test folder
   ],
+  coverageThreshold: {
+    "global": {
+      "statements": 80,
+      "branches": 75,
+      "functions": 80,
+      "lines": 80
+    }
+  }
 };
 
 export default config;
