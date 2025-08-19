6f98983abc02820fd7c729207d209f47ad78b00e
Prosvirin Vladimir <vlprosvirin@gmail.com>
2025-08-19 16:02:25 +0300

    Update behaviour test

--- apps/smart-forms-app/src/tests/questionnaireRenderer.test.tsx
+++ apps/smart-forms-app/src/tests/questionnaireRenderer.test.tsx
@@ -1,7 +1,52 @@
+import { test, vi } from 'vitest';
 import { render } from '@testing-library/react';
-import { test } from 'vitest';
-import { BaseRenderer } from '@aehrc/smart-forms-renderer';
+import type { Questionnaire, QuestionnaireResponse } from 'fhir/r4';
+import aboriginalForm from '../data/resources/Questionnaire/Questionnaire-AboriginalTorresStraitIslanderHealthCheckAssembled-0.1.0.json';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+
+import {
+  BaseRenderer,
+  RendererThemeProvider,
+  useBuildForm,
+  useRendererQueryClient
+} from '@aehrc/smart-forms-renderer';
+
+vi.mock('fhirclient', () => ({
+  client: () => ({
+    request: vi.fn(() => Promise.resolve({}))
+  })
+}));
 
 test('Basic component renders correctly', async () => {
-  render(<BaseRenderer />);
+  const form = aboriginalForm as Questionnaire;
+
+  const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  expect(container.innerHTML).toContain('Patient Details')
+
 });
+
+interface BuildFormWrapperForStorybookProps {
+  questionnaire: Questionnaire;
+  questionnaireResponse?: QuestionnaireResponse;
+}
+
+function BuildFormWrapperForStorybook(props: BuildFormWrapperForStorybookProps) {
+  const { questionnaire, questionnaireResponse } = props;
+
+  const queryClient = useRendererQueryClient();
+  const isBuilding = useBuildForm(questionnaire, questionnaireResponse, undefined, 'https://r4.ontoserver.csiro.au/fhir');
+
+  if (isBuilding) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <RendererThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <BaseRenderer />
+      </QueryClientProvider>
+    </RendererThemeProvider>
+  );
+} 
