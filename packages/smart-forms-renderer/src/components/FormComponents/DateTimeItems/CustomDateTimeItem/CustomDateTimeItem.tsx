0c36a13b85678f52de084dd78bbc87614450fab7
Sean Fong <sean.fong@csiro.au>
2025-09-15 15:30:13 +0930

    Revert to more tolerant code while applying "valueDateFhir" assignment change

--- packages/smart-forms-renderer/src/components/FormComponents/DateTimeItems/CustomDateTimeItem/CustomDateTimeItem.tsx
+++ packages/smart-forms-renderer/src/components/FormComponents/DateTimeItems/CustomDateTimeItem/CustomDateTimeItem.tsx
@@ -70,20 +70,20 @@ function CustomDateTimeItem(props: BaseItemProps) {
   // Extract valueDateTimeFhir, dateTimeDayJs and valueDateFhir from qrDateTime
   if (qrDateTime.answer) {
     if (qrDateTime.answer[0].valueDate) {
-      valueDateFhir = qrDateTime.answer[0].valueDate;
+      // NOTE: valueDate will not contain time, but we should handle it just in case
+      valueDateTimeFhir = qrDateTime.answer[0].valueDate;
     } else if (qrDateTime.answer[0].valueDateTime) {
       valueDateTimeFhir = qrDateTime.answer[0].valueDateTime;
-      
-      // Parse the full DateTime string for time extraction
-      if (valueDateTimeFhir.includes('T')) {
-        // Extract date part for date field
-        valueDateFhir = valueDateTimeFhir.split('T')[0];
-        // Parse the full DateTime string for time extraction
-        dateTimeDayJs = dayjs(valueDateTimeFhir);
-      } else {
-        // valueDateTimeFhir does not contain "T", that means it's a date only
-        valueDateFhir = valueDateTimeFhir;
-      }
+    }
+
+    // Split date and time at "T", 2015-02-07T13:28:17-05:00
+    if (valueDateTimeFhir.includes('T')) {
+      valueDateFhir = valueDateTimeFhir.split('T')[0];
+      dateTimeDayJs = dayjs(valueDateTimeFhir);
+    }
+    // valueDateTimeFhir does not contain "T", that means it's a date only
+    else {
+      valueDateFhir = valueDateTimeFhir;
     }
   }
 
