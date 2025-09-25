977f7f4349cf7106d32f77c33297d513dc1280f8
Prosvirin Vladimir <vlprosvirin@gmail.com>
2025-09-26 00:23:50 +0300

    Update behaviour test and vitest.config

--- apps/smart-forms-app/src/test/questionnaireRenderer.test.tsx
+++ apps/smart-forms-app/src/test/questionnaireRenderer.test.tsx
@@ -19,80 +19,50 @@ vi.mock('fhirclient', () => ({
   })
 }));
 
-export async function getAnswerRecurcise(linkId: string) {
+export async function getAnswerRecurciseByLabel(text: string) {
   const qr = questionnaireResponseStore.getState().updatableResponse;
-
+  console.log(qr);
   const result = await evaluate(
     qr,
-    `QuestionnaireResponse.repeat(item).where(linkId='${linkId}').answer`
+    `QuestionnaireResponse.repeat(item).where((text = '${text}')).answer`
   );
   return result;
 }
 
-test('Basic component renders correctly', async () => {
+test('Behavior test', async () => {
   const form = aboriginalForm as Questionnaire;
+
   const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
   await new Promise((resolve) => setTimeout(resolve, 1000));
-
   expect(container.innerHTML).toContain('Patient Details');
-});
-
-test('Add name into name field', async () => {
-  const form = aboriginalForm as Questionnaire;
-  const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
-  const nameLinkId = '17596726-34cf-4133-9960-7081e1d63558';
-  const nameText = 'David';
-
-  await inputText(container, nameLinkId, nameText);
-
-  const result = await getAnswerRecurcise(nameLinkId);
+  const nameTex = 'David';
+  await inputText(container, 'Name', nameTex);
+  let result = await getAnswerRecurciseByLabel('Name');
   expect(result).toHaveLength(1);
-  expect(result[0]).toEqual(expect.objectContaining({ valueString: nameText }));
-});
+  expect(result[0]).toEqual(expect.objectContaining({ valueString: nameTex }));
 
-test('Add date into date field', async () => {
-  const form = aboriginalForm as Questionnaire;
-  const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
-  const dateLinkId = '90ad8f16-16e4-4438-a7aa-b3189f510da2';
   const date = '11/11/2021';
   const dateText = '2021-11-11';
-
-  await inputDate(container, dateLinkId, date);
-
-  const result = await getAnswerRecurcise(dateLinkId);
+  await inputDate(container, 'Date of birth', date);
+  result = await getAnswerRecurciseByLabel('Date of birth');
   expect(result).toHaveLength(1);
   expect(result[0]).toEqual(expect.objectContaining({ valueDate: dateText }));
-});
 
-test('Add age into age field', async () => {
-  const form = aboriginalForm as Questionnaire;
-  const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
-  const ageLinkId = 'e2a16e4d-2765-4b61-b286-82cfc6356b30';
   const ageText = 24;
-
-  await inputInteger(container, ageLinkId, ageText);
-
-  const result = await getAnswerRecurcise(ageLinkId);
+  await inputInteger(container, 'Age', ageText);
+  result = await getAnswerRecurciseByLabel('Age');
   expect(result).toHaveLength(1);
   expect(result[0]).toEqual(expect.objectContaining({ valueInteger: ageText }));
-});
 
-test('Registered for NDIS opening NDIS number field', async () => {
-  const form = aboriginalForm as Questionnaire;
-  const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
   const answerCoding = {
     code: 'Y',
     display: 'Yes',
     system: 'http://terminology.hl7.org/CodeSystem/v2-0532'
   };
-  const radioLinkId = '924b4500-53ac-4c4e-831b-7ab5569ff981';
-  const inputLinkId = '7379a0fd-d95b-4ecb-a781-9f43d1394f10';
   const inputTargetText = 'Some text';
-
-  await checkRadioOption(container, radioLinkId, answerCoding.display);
-  await inputText(container, inputLinkId, inputTargetText);
-
-  const result = await getAnswerRecurcise(inputLinkId);
+  await checkRadioOption(container, 'Registered for NDIS', answerCoding.display);
+  await inputText(container, 'NDIS Number', inputTargetText);
+  result = await getAnswerRecurciseByLabel('NDIS Number');
   expect(result).toHaveLength(1);
   expect(result[0]).toEqual(expect.objectContaining({ valueString: inputTargetText }));
 });
@@ -104,7 +74,6 @@ interface BuildFormWrapperForStorybookProps {
 
 function BuildFormWrapperForStorybook(props: BuildFormWrapperForStorybookProps) {
   const { questionnaire, questionnaireResponse } = props;
-
   const queryClient = useRendererQueryClient();
   const isBuilding = useBuildForm(
     questionnaire,
--- apps/smart-forms-app/vitest.config.ts
+++ apps/smart-forms-app/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    testTimeout:40000,
     environment: 'jsdom',
     include: ['**/__tests__/**/*.{test,spec}.{js,ts,jsx,tsx}', '**/*.{test,spec}.{js,ts,jsx,tsx}'],
     exclude: ['**/e2e/**', '**/node_modules/**'],
