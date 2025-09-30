1e89ba329c4d5adb90f0708eefe091e6208b4b98
Prosvirin Vladimir <vlprosvirin@gmail.com>
2025-09-30 23:35:22 +0300

    Add expect type dep, add vitest types in tsconfig and update behaviour test

--- apps/smart-forms-app/src/test/questionnaireRenderer.test.tsx
+++ apps/smart-forms-app/src/test/questionnaireRenderer.test.tsx
@@ -1,10 +1,10 @@
-import { test, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import { evaluate } from 'fhirpath';
 import type { Questionnaire, QuestionnaireResponse } from 'fhir/r4';
 import aboriginalForm from '../data/resources/Questionnaire/Questionnaire-AboriginalTorresStraitIslanderHealthCheckAssembled-0.1.0.json';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { testUtils } from '@aehrc/smart-forms-renderer';
+import { vi } from 'vitest';
 
 import {
   BaseRenderer,
@@ -21,9 +21,9 @@ vi.mock('fhirclient', () => ({
   })
 }));
 
-export async function getAnswerRecurciseByLabel(text: string) {
+export async function getAnswerRecursiveByLabel(text: string) {
   const qr = questionnaireResponseStore.getState().updatableResponse;
-  console.log(qr);
+  console.log(1);
   const result = await evaluate(
     qr,
     `QuestionnaireResponse.repeat(item).where((text = '${text}')).answer`
@@ -31,42 +31,18 @@ export async function getAnswerRecurciseByLabel(text: string) {
   return result;
 }
 
-test('Behavior test', async () => {
+test('behaviour-test-example', async () => {
   const form = aboriginalForm as Questionnaire;
 
   const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
   await new Promise((resolve) => setTimeout(resolve, 1000));
   expect(container.innerHTML).toContain('Patient Details');
-  const nameTex = 'David';
-  await inputText(container, 'Name', nameTex);
-  let result = await getAnswerRecurciseByLabel('Name');
-  expect(result).toHaveLength(1);
-  expect(result[0]).toEqual(expect.objectContaining({ valueString: nameTex }));
 
-  const date = '11/11/2021';
-  const dateText = '2021-11-11';
-  await inputDate(container, 'Date of birth', date);
-  result = await getAnswerRecurciseByLabel('Date of birth');
-  expect(result).toHaveLength(1);
-  expect(result[0]).toEqual(expect.objectContaining({ valueDate: dateText }));
-
-  const ageText = 24;
-  await inputInteger(container, 'Age', ageText);
-  result = await getAnswerRecurciseByLabel('Age');
-  expect(result).toHaveLength(1);
-  expect(result[0]).toEqual(expect.objectContaining({ valueInteger: ageText }));
-
-  const answerCoding = {
-    code: 'Y',
-    display: 'Yes',
-    system: 'http://terminology.hl7.org/CodeSystem/v2-0532'
-  };
-  const inputTargetText = 'Some text';
-  await checkRadioOption(container, 'Registered for NDIS', answerCoding.display);
-  await inputText(container, 'NDIS Number', inputTargetText);
-  result = await getAnswerRecurciseByLabel('NDIS Number');
-  expect(result).toHaveLength(1);
-  expect(result[0]).toEqual(expect.objectContaining({ valueString: inputTargetText }));
+  await inputText(container, 'Name', 'David');
+  await inputDate(container, 'Date of birth', '11/11/2021');
+  await inputInteger(container, 'Age', 24);
+  await checkRadioOption(container, 'Registered for NDIS', 'Yes');
+  await inputText(container, 'NDIS Number', 'Some text');
 });
 
 interface BuildFormWrapperForStorybookProps {
