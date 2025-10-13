812d95cc80ca6ab5a357751970c2061bbe08a934
Sean Fong <sean.fong@csiro.au>
2025-10-13 19:49:03 +1030

    Fix package.json and make questionnaireRenderer.test.tsx tests more robust

--- apps/smart-forms-app/src/test/questionnaireRenderer.test.tsx
+++ apps/smart-forms-app/src/test/questionnaireRenderer.test.tsx
@@ -1,15 +1,15 @@
-import { render } from '@testing-library/react';
+import { act, render, waitFor } from '@testing-library/react';
 import { evaluate } from 'fhirpath';
 import type { Questionnaire, QuestionnaireResponse } from 'fhir/r4';
 import aboriginalForm from '../data/resources/Questionnaire/Questionnaire-AboriginalTorresStraitIslanderHealthCheckAssembled-0.1.0.json';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { testUtils } from '@aehrc/smart-forms-renderer';
 import { vi } from 'vitest';
 
 import {
   BaseRenderer,
   questionnaireResponseStore,
   RendererThemeProvider,
+  testUtils,
   useBuildForm,
   useRendererQueryClient
 } from '@aehrc/smart-forms-renderer';
@@ -35,14 +35,15 @@ test('behaviour-test-example', async () => {
   const form = aboriginalForm as Questionnaire;
 
   const { container } = render(<BuildFormWrapperForStorybook questionnaire={form} />);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  expect(container.innerHTML).toContain('Patient Details');
+  await waitFor(() => expect(container.innerHTML).toContain('Patient Details'));
 
-  await inputText(container, 'Name', 'David');
-  await inputDate(container, 'Date of birth', '11/11/2021');
-  await inputInteger(container, 'Age', 24);
-  await checkRadioOption(container, 'Registered for NDIS', 'Yes');
-  await inputText(container, 'NDIS Number', 'Some text');
+  await act(async () => {
+    await inputText(container, 'Name', 'David');
+    await inputDate(container, 'Date of birth', '11/11/2021');
+    await inputInteger(container, 'Age', 24);
+    await checkRadioOption(container, 'Registered for NDIS', 'Yes');
+    await inputText(container, 'NDIS Number', 'Some text');
+  });
 });
 
 interface BuildFormWrapperForStorybookProps {
@@ -53,12 +54,11 @@ interface BuildFormWrapperForStorybookProps {
 function BuildFormWrapperForStorybook(props: BuildFormWrapperForStorybookProps) {
   const { questionnaire, questionnaireResponse } = props;
   const queryClient = useRendererQueryClient();
-  const isBuilding = useBuildForm(
+  const isBuilding = useBuildForm({
     questionnaire,
     questionnaireResponse,
-    undefined,
-    'https://r4.ontoserver.csiro.au/fhir'
-  );
+    terminologyServerUrl: 'https://r4.ontoserver.csiro.au/fhir'
+  });
 
   if (isBuilding) {
     return <div>Loading...</div>;
