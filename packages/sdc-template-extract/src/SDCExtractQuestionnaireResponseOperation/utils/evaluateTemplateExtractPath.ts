902e4ca64d16bae24272c453031d35c29a6ea39a
Leonie Dickson <leonie.dickson@csiro.au>
2025-11-17 15:36:31 +1100

    fix treatment of arrays in template-based extract

--- packages/sdc-template-extract/src/SDCExtractQuestionnaireResponseOperation/utils/evaluateTemplateExtractPath.ts
+++ packages/sdc-template-extract/src/SDCExtractQuestionnaireResponseOperation/utils/evaluateTemplateExtractPath.ts
@@ -1,10 +1,10 @@
 import type { FhirResource, OperationOutcomeIssue, QuestionnaireResponse } from 'fhir/r4';
 import { fhirPathEvaluate } from './fhirpathEvaluate';
 import type { TemplateExtractPath } from '../interfaces/templateExtractPath.interface';
-import { getCombinedExpression } from './expressionManipulation';
+import { getCombinedExpression, stripTrailingIndexFromPath } from './expressionManipulation';
 import { insertValuesToPath } from './templateInsert';
 import type { EntryPathPosition } from '../interfaces/entryPathPosition.interface';
-
+import { addCurrentEntryPathPosition, getStartingIndex } from './entryPathPosition';
 /**
  * Evaluates FHIRPath expressions from a value map and inserts the resulting values into the target template.
  *
@@ -39,34 +39,64 @@ export function evaluateAndInsertIntoPath(
     : targetQRItemFhirPathWithIndex;
 
   // Evaluate context path to get the context result
-  const contextResult = fhirPathEvaluate({
+  const contextResults = fhirPathEvaluate({
     fhirData: questionnaireResponse,
     path: contextPath,
     envVars: fhirPathContext,
     warnings: populateIntoTemplateWarnings
   });
 
-  for (const [valuePath, valueEvaluation] of valuePathMap.entries()) {
-    // Use context result to evaluate the value expression
-    const valueResult = fhirPathEvaluate({
-      fhirData: contextResult,
-      path: valueEvaluation.valueExpression,
-      envVars: fhirPathContext,
-      warnings: populateIntoTemplateWarnings
-    });
+  // Get the index that we should start inserting values at for this entry path
+  const baseEntryPath = stripTrailingIndexFromPath(entryPath);
+  const startingIndex = getStartingIndex(baseEntryPath, entryPath, entryPathPositionMap);
 
-    // Insert values to the specified entry path
-    insertValuesToPath(
-      entryPath,
-      valuePath,
-      valueResult,
-      entryPathPositionMap,
-      templateToMutate,
-      cleanTemplate,
-      populateIntoTemplateWarnings
-    );
+  // Iterate over context results to evaluate values and insert into template
+  for (let i = 0; i < contextResults.length; i++) {
+    const contextResult = contextResults[i];
+    const insertIndex = startingIndex + i;
 
-    // Collect value evaluation results for debugging
-    valueEvaluation.valueResult = valueResult;
+    // Flag to track if this is a new insert - used to determine the correct behaviour when inserting into an array element
+    // The first time we see a context result, we add a new entry at the specified index
+    // Subsequent inserts for the same context result will merge into the same index
+    let isNewInsert = true;
+
+    for (const [valuePath, valueEvaluation] of valuePathMap.entries()) {
+      // Use context result to evaluate the value expression
+      const valueResult = fhirPathEvaluate({
+        fhirData: contextResult,
+        path: valueEvaluation.valueExpression,
+        envVars: fhirPathContext,
+        warnings: populateIntoTemplateWarnings
+      });
+
+      // Insert values to the specified entry path
+      insertValuesToPath(
+        entryPath,
+        insertIndex,
+        isNewInsert,
+        valuePath,
+        valueResult,
+        templateToMutate,
+        cleanTemplate,
+        populateIntoTemplateWarnings
+      );
+
+      if (Array.isArray(valueResult) && valueResult.length) {
+        // Values found, set flag to false so that subsequent inserts for the same context are now merged at the same index
+        isNewInsert = false;
+      }
+
+      // Collect value evaluation results for debugging
+      valueEvaluation.valueResult.push(valueResult);
+    }
   }
+
+  // Track the number of inserted entries for the base entry path using entryPathPositionMap
+  addCurrentEntryPathPosition(
+    baseEntryPath,
+    entryPath,
+    startingIndex,
+    contextResults.length,
+    entryPathPositionMap
+  );
 }
