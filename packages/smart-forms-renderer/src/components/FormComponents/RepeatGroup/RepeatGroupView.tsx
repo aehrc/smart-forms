a1751a319d007f56b3a5d9db0b5b6dfe57b2903c
Clinton Gillespie <clinton.gillespie@csiro.au>
2026-03-27 14:49:00 +1000

    remove debugging text

--- packages/smart-forms-renderer/src/components/FormComponents/RepeatGroup/RepeatGroupView.tsx
+++ packages/smart-forms-renderer/src/components/FormComponents/RepeatGroup/RepeatGroupView.tsx
@@ -100,7 +100,6 @@ function RepeatGroupView(props: RepeatGroupViewProps) {
         <AccordionDetails sx={{ pt: 0 }}>
           {itemTextToDisplay ? <Divider sx={{ mb: 1.5, opacity: 0.6 }} /> : null}
           <TransitionGroup>
-            this is the mapping of the repeat group
             {repeatGroups.map(({ id, qrItem: nullableQrItem }, index) => {
               const answeredQrItem = createEmptyQrItem(qItem, undefined);
               if (nullableQrItem) {
